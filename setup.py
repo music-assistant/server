844626ad5bb1e1e92c9f9abab9434790a9795489
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-04-16 10:26:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,38 @@
-# Upload to PyPI Live
-# sudo python3 setup.py sdist bdist_wheel
-# sudo python3 -m twine upload dist/*
+from pathlib import Path
 
-import os
+from setuptools import find_packages, setup
 
-import setuptools
-
+PROJECT_DIR = Path(__file__).parent.resolve()
+README_FILE = PROJECT_DIR / "README.md"
 VERSION = "0.0.20"
-NAME = "music_assistant"
-
-with open("README.md", "r") as fh:
-    LONG_DESC = fh.read()
 
 with open("requirements.txt") as f:
     INSTALL_REQUIRES = f.read().splitlines()
 if os.name != "nt":
     INSTALL_REQUIRES.append("uvloop")
 
-setuptools.setup(
-    name=NAME,
+setup(
+    name="music_assistant",
     version=VERSION,
+    url="https://github.com/marcelveldt/musicassistant",
+    download_url="https://github.com/marcelveldt/musicassistant",
     author="Marcel van der Veldt",
-    author_email="marcelveldt@users.noreply.github.com",
+    author_email="m.vanderveldt@outlook.com",
     description="Music library manager and player based on sox.",
-    long_description=LONG_DESC,
+    long_description=README_FILE.read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    url="https://github.com/marcelveldt/musicassistant.git",
-    packages=["music_assistant"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
+    packages=find_packages(exclude=["test.*", "test"]),
+    python_requires=">=3.7",
+    include_package_data=True,
     install_requires=INSTALL_REQUIRES,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Home Automation",
+    ],
 )
