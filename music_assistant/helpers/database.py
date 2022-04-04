3f9655cf9eab1c710443af179b8f84c7e38fc4d4
Marcel van der Veldt <m.vanderveldt@outlook.com>
2022-04-05 00:04:02 +0200

    Update database.py

--- music_assistant/helpers/database.py
+++ music_assistant/helpers/database.py
@@ -1,6 +1,7 @@
 """Database logic."""
 from __future__ import annotations
-
+import asyncio
+from contextlib import asynccontextmanager
 from typing import Any, Dict, List, Mapping
 
 from databases import Database as Db
@@ -9,89 +10,116 @@ from databases import DatabaseURL
 from music_assistant.constants import EventType
 from music_assistant.helpers.typing import EventDetails, MusicAssistant
 
+# pylint: disable=invalid-name
 
-class Database(Db):
+
+class Database:
     """Class that holds the (logic to the) database."""
 
     def __init__(self, mass: MusicAssistant, url: DatabaseURL):
         """Initialize class."""
-        super().__init__(url, timeout=360)
+        self.url = url
         self.mass = mass
         self.logger = mass.logger.getChild("db")
-        # self._lock = asyncio.Lock()
+        self._lock = asyncio.Lock()
         mass.subscribe(self.__on_shutdown_event, EventType.SHUTDOWN)
 
     async def setup(self):
         """Async initialize of module."""
-        await self.connect()
+        # await self.connect()
+
+    @asynccontextmanager
+    async def get_db(self, db: Db | None = None) -> Db:
+        """Context manager helper to get the active db connection."""
+        if db is not None:
+            yield db
+        else:
+            async with Db(self.url) as _db:
+                yield _db
 
     async def get_rows(
-        self, table: str, match: dict = None, order_by: str = None
+        self, table: str, match: dict = None, order_by: str = None, db: Db | None = None
     ) -> List[Mapping]:
         """Get all rows for given table."""
-        sql_query = f"SELECT * FROM {table}"
-        if match is not None:
-            sql_query += " WHERE " + " AND ".join((f"{x} = :{x}" for x in match))
-        if order_by is not None:
-            sql_query += f"ORDER BY {order_by}"
-        return await self.fetch_all(sql_query, match)
+        async with self.get_db(db) as _db:
+            sql_query = f"SELECT * FROM {table}"
+            if match is not None:
+                sql_query += " WHERE " + " AND ".join((f"{x} = :{x}" for x in match))
+            if order_by is not None:
+                sql_query += f"ORDER BY {order_by}"
+            return await _db.fetch_all(sql_query, match)
 
-    async def get_rows_from_query(self, query: str) -> List[Mapping]:
+    async def get_rows_from_query(
+        self, query: str, db: Db | None = None
+    ) -> List[Mapping]:
         """Get all rows for given custom query."""
-        return await self.fetch_all(query)
+        async with self.get_db(db) as _db:
+            return await _db.fetch_all(query)
 
     async def search(
-        self, table: str, search: str, column: str = "name"
+        self, table: str, search: str, column: str = "name", db: Db | None = None
     ) -> List[Mapping]:
         """Search table by column."""
-        sql_query = f'SELECT * FROM {table} WHERE {column} LIKE "{search}"'
-        return await self.fetch_all(sql_query)
+        async with self.get_db(db) as _db:
+            sql_query = f'SELECT * FROM {table} WHERE {column} LIKE "{search}"'
+            return await _db.fetch_all(sql_query)
 
-    async def get_row(self, table: str, match: Dict[str, Any] = None) -> Mapping | None:
+    async def get_row(
+        self, table: str, match: Dict[str, Any] = None, db: Db | None = None
+    ) -> Mapping | None:
         """Get single row for given table where column matches keys/values."""
-        sql_query = f"SELECT * FROM {table} WHERE "
-        sql_query += " AND ".join((f"{x} = :{x}" for x in match))
-        return await self.fetch_one(sql_query, match)
+        async with self.get_db(db) as _db:
+            sql_query = f"SELECT * FROM {table} WHERE "
+            sql_query += " AND ".join((f"{x} = :{x}" for x in match))
+            return await _db.fetch_one(sql_query, match)
 
-    async def insert_or_replace(self, table: str, values: Dict[str, Any]) -> Mapping:
+    async def insert_or_replace(
+        self, table: str, values: Dict[str, Any], db: Db | None = None
+    ) -> Mapping:
         """Insert or replace data in given table."""
-        keys = tuple(values.keys())
-        sql_query = f'INSERT OR REPLACE INTO {table}({",".join(keys)})'
-        sql_query += f' VALUES ({",".join((f":{x}" for x in keys))})'
-        await self.execute(sql_query, values)
-        # return inserted/replaced item
-        lookup_vals = {
-            key: value
-            for key, value in values.items()
-            if value is not None and value != ""
-        }
-        return await self.get_row(table, lookup_vals)
+        async with self.get_db(db) as _db:
+            keys = tuple(values.keys())
+            sql_query = f'INSERT OR REPLACE INTO {table}({",".join(keys)})'
+            sql_query += f' VALUES ({",".join((f":{x}" for x in keys))})'
+            await _db.execute(sql_query, values)
+            # return inserted/replaced item
+            lookup_vals = {
+                key: value
+                for key, value in values.items()
+                if value is not None and value != ""
+            }
+            return await self.get_row(table, lookup_vals, db=_db)
 
     async def update(
-        self, table: str, match: Dict[str, Any], values: Dict[str, Any]
+        self,
+        table: str,
+        match: Dict[str, Any],
+        values: Dict[str, Any],
+        db: Db | None = None,
     ) -> Mapping:
         """Update record."""
-        keys = tuple(values.keys())
-        sql_query = f'UPDATE {table} SET {",".join((f"{x}=:{x}" for x in keys))} WHERE '
-        sql_query += " AND ".join((f"{x} = :{x}" for x in match))
-        await self.execute(sql_query, {**match, **values})
-        # return updated item
-        return await self.get_row(table, match)
+        async with self.get_db(db) as _db:
+            keys = tuple(values.keys())
+            sql_query = (
+                f'UPDATE {table} SET {",".join((f"{x}=:{x}" for x in keys))} WHERE '
+            )
+            sql_query += " AND ".join((f"{x} = :{x}" for x in match))
+            await _db.execute(sql_query, {**match, **values})
+            # return updated item
+            return await self.get_row(table, match, db=_db)
 
-    async def delete(self, table: str, match: Dict[str, Any]) -> None:
+    async def delete(
+        self, table: str, match: Dict[str, Any], db: Db | None = None
+    ) -> None:
         """Delete data in given table."""
-        sql_query = f"DELETE FROM {table}"
-        sql_query += " WHERE " + " AND ".join((f"{x} = :{x}" for x in match))
-        await self.execute(sql_query)
-
-    # async def execute(self, query: ClauseElement | str, values: dict = None) -> Any:
-    #     """Execute query on database."""
-    #     async with self._lock:
-    #         await super().execute(query, values)
+        async with self.get_db(db) as _db:
+            sql_query = f"DELETE FROM {table}"
+            sql_query += " WHERE " + " AND ".join((f"{x} = :{x}" for x in match))
+            await _db.execute(sql_query)
 
     async def __on_shutdown_event(
         self, event: EventType, details: EventDetails
     ) -> None:
         """Handle shutdown event."""
-        await self.disconnect()
+        # await self.disconnect()
         self.logger.info("database closed")
