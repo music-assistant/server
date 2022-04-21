d2770d4c2693d17e09f03f1a7cd27048e5fade46
Marcel van der Veldt <m.vanderveldt@outlook.com>
2022-04-21 20:37:30 +0200

    fix typo in logger

--- music_assistant/mass.py
+++ music_assistant/mass.py
@@ -135,7 +135,7 @@ class MusicAssistant:
     ) -> None:
         """Add job to be (slowly) processed in the background (one by one)."""
         if not allow_duplicate and name in self._job_names:
-            self.logger("Ignoring job %s because it is already in the queue", name)
+            self.logger.debug("Ignored duplicate job: %s", name)
             return
         if not name:
             name = job.__qualname__ or job.__name__
