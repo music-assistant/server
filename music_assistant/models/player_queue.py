53cce166b573fea266ccdbc5a2b68f140d02ec22
Marcel van der Veldt <m.vanderveldt@outlook.com>
2022-05-31 15:02:28 +0200

    power on player should happen in player implementation

--- music_assistant/models/player_queue.py
+++ music_assistant/models/player_queue.py
@@ -288,9 +288,6 @@ class PlayerQueue:
 
     async def play_index(self, index: Union[int, str], passive: bool = False) -> None:
         """Play item at index (or item_id) X in queue."""
-        # power on player when requesting play
-        if not self.player.powered:
-            await self.player.power(True)
         if self.player.use_multi_stream:
             await self.mass.streams.stop_multi_client_queue_stream(self.queue_id)
         if not isinstance(index, int):
