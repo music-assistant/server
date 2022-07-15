3541e91ea21b646b71d25c8a8b910ad9cd915eda
Marcel van der Veldt <m.vanderveldt@outlook.com>
2022-07-15 22:50:02 +0200

    Update music_assistant/music_providers/spotify.py
    Co-authored-by: Marvin Schenkel <marvinschenkel@gmail.com>

--- music_assistant/music_providers/spotify.py
+++ music_assistant/music_providers/spotify.py
@@ -65,6 +65,7 @@ class SpotifyProvider(MusicProvider):
             MusicProviderFeature.LIBRARY_ALBUMS_EDIT,
             MusicProviderFeature.LIBRARY_PLAYLISTS_EDIT,
             MusicProviderFeature.LIBRARY_RADIOS_EDIT,
+            MusicProviderFeature.LIBRARY_TRACKS_EDIT,
             MusicProviderFeature.PLAYLIST_TRACKS_EDIT,
             MusicProviderFeature.BROWSE,
             MusicProviderFeature.SEARCH,
