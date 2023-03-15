6f78c2963fd513877171b4eda37b165814f4513e
Jonathan <jonathan@bangert.dk>
2023-03-15 13:43:27 +0100

    tf

--- music_assistant/server/providers/deezer/deezer-test.py
+++ music_assistant/server/providers/deezer/deezer-test.py
@@ -1,4 +1,33 @@
-import deezer
+import asyncio
+import json
 
-client = deezer.Client(app_id=587964, app_secret="3725582e5aeec225901e4eb03684dbfb", access_token="fre7UrzZBAOzCCeHJuGcYbzGtwFCMhvTNJWHNy5jJveyMjD3wBk")
+import requests
 
+
+async def get_sid():
+    return requests.get(
+        "http://www.deezer.com/ajax/gw-light.php",
+        params={"method": "deezer.ping", "api_version": "1.0", "api_token": ""},
+    ).json()["results"]["SESSION"]
+
+
+async def get_user_data(sid):
+    return requests.get("https://www.deezer.com/ajax/gw-light.php?method=$method&input=3&api_version=1.0&api_token=$apiToken" --header "Cookie: sid=$session")
+
+
+# main coroutine
+async def main():
+    # start executing a shell command in a subprocess
+    process = await asyncio.create_subprocess_shell(
+        "./dzr-url 1518077822", stdout=asyncio.subprocess.PIPE
+    )
+    # report the details of the subprocess
+    print(f"subprocess: {process}")
+    # read a line of output from the program
+    data, _ = await process.communicate()
+    # report the data
+    print(data)
+
+
+# entry point
+asyncio.run(get_sid())
