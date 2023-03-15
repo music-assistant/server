127c94d68822db872e35fe62c60cc00eb3c39916
Jonathan Bangert <jonathan@bangert.dk>
2023-03-15 18:17:01 +0100

    Finally a port

--- music_assistant/server/providers/deezer/test.sh
+++ music_assistant/server/providers/deezer/test.sh
@@ -4,3 +4,5 @@ APP_WEB=$($FETCH -L deezer.com/en/channels/explore | sed -n 's/.*src="\([^"]*app
 TMP_CBC=$(echo "$APP_WEB" | tr ,_ '\n' | sed -n 's/.*\(%5B0x[63]1%.\{48\}%5D\).*/\1/p' | sed 's/%5[BD]//g;s/%/\\x/g' | xargs -0 printf %b | sed 's/0x/\\x/g' | tr , '\n' | xargs -0 printf %b)
 DZR_CBC=$(unscramble $TMP_CBC);
 echo -e $DZR_CBC
+
+curl -s https://www.deezer.com/ajax/gw-light.php?method=song.getListData&input=3&api_version=1.0&api_token=4sRb11qHYFgDr5asHpp~QV1G2Oj867UQ --header "Cookie: sid=freba961464ed3974f08015d536de9e3d015a519" --data "{\"sng_ids\":[2062992067,505204262,2175377377]}"
