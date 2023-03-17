41618afc15a6168440a19d70b34903d3da6d1d75
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-03-17 10:12:30 +0100

    Fix resolving of queue item images

--- music_assistant/server/controllers/metadata.py
+++ music_assistant/server/controllers/metadata.py
@@ -249,8 +249,6 @@ class MetaDataController:
         img_path = await self.get_image_url_for_item(
             media_item=media_item,
             img_type=img_type,
-            allow_local=True,
-            local_as_base64=False,
         )
         if not img_path:
             return None
@@ -260,43 +258,39 @@ class MetaDataController:
         self,
         media_item: MediaItemType,
         img_type: ImageType = ImageType.THUMB,
-        allow_local: bool = True,
-        local_as_base64: bool = False,
+        resolve_local: bool = True,
     ) -> str | None:
         """Get url to image for given media media_item."""
         if not media_item:
             return None
         if isinstance(media_item, ItemMapping):
             media_item = await self.mass.music.get_item_by_uri(media_item.uri)
-        if media_item and media_item.metadata.images:
-            for img in media_item.metadata.images:
-                if img.type != img_type:
-                    continue
-                if img.is_file and not allow_local:
-                    continue
-                if img.is_file and local_as_base64:
-                    # return base64 string of the image (compatible with browsers)
-                    return await self.get_thumbnail(img.url, base64=True)
-                return img.url
+        for allow_local in (False, True):
+            if media_item and media_item.metadata.images:
+                for img in media_item.metadata.images:
+                    if img.type != img_type:
+                        continue
+                    if img.is_file and not allow_local:
+                        continue
+                    if img.is_file and resolve_local:
+                        # return imageproxy url for local filesystem items
+                        # the original path is double encoded
+                        encoded_url = urllib.parse.quote(urllib.parse.quote(img.url))
+                        return f"{self.mass.base_url}/imageproxy?path={encoded_url}"
+                    return img.url
 
-        # retry with track's album
-        if media_item.media_type == MediaType.TRACK and media_item.album:
-            return await self.get_image_url_for_item(
-                media_item.album, img_type, allow_local, local_as_base64
-            )
+            # retry with track's album
+            if media_item.media_type == MediaType.TRACK and media_item.album:
+                return await self.get_image_url_for_item(media_item.album, img_type, resolve_local)
 
-        # try artist instead for albums
-        if media_item.media_type == MediaType.ALBUM and media_item.artist:
-            return await self.get_image_url_for_item(
-                media_item.artist, img_type, allow_local, local_as_base64
-            )
+            # try artist instead for albums
+            if media_item.media_type == MediaType.ALBUM and media_item.artist:
+                return await self.get_image_url_for_item(media_item.artist, img_type, resolve_local)
 
-        # last resort: track artist(s)
-        if media_item.media_type == MediaType.TRACK and media_item.artists:
-            for artist in media_item.artists:
-                return await self.get_image_url_for_item(
-                    artist, img_type, allow_local, local_as_base64
-                )
+            # last resort: track artist(s)
+            if media_item.media_type == MediaType.TRACK and media_item.artists:
+                for artist in media_item.artists:
+                    return await self.get_image_url_for_item(artist, img_type, resolve_local)
 
         return None
 
--- music_assistant/server/helpers/didl_lite.py
+++ music_assistant/server/helpers/didl_lite.py
@@ -31,14 +31,15 @@ def create_didl_metadata(url: str, queue_item: QueueItem, flow_mode: bool = Fals
             "</item>"
             "</DIDL-Lite>"
         )
+
     if is_radio:
         # radio or other non-track item
-        image = queue_item.image.url if queue_item.image else ""
+        image = queue_item.image_url if queue_item.image else ""
         return (
             '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/">'
             f'<item id="{queue_item.queue_item_id}" parentID="0" restricted="1">'
             f"<dc:title>{_escape_str(queue_item.name)}</dc:title>"
-            f"<upnp:albumArtURI>{queue_item.image.url}</upnp:albumArtURI>"
+            f"<upnp:albumArtURI>{queue_item.image_url}</upnp:albumArtURI>"
             f"<dc:queueItemId>{image}</dc:queueItemId>"
             "<upnp:class>object.item.audioItem.audioBroadcast</upnp:class>"
             f"<upnp:mimeType>audio/{ext}</upnp:mimeType>"
@@ -55,7 +56,6 @@ def create_didl_metadata(url: str, queue_item: QueueItem, flow_mode: bool = Fals
         album = _escape_str(queue_item.media_item.album.name)
     else:
         album = ""
-    image = queue_item.image.url if queue_item.image else ""
     item_class = "object.item.audioItem.musicTrack"
     duration_str = str(datetime.timedelta(seconds=queue_item.duration))
     return (
@@ -68,7 +68,7 @@ def create_didl_metadata(url: str, queue_item: QueueItem, flow_mode: bool = Fals
         f"<upnp:duration>{queue_item.duration}</upnp:duration>"
         "<upnp:playlistTitle>Music Assistant</upnp:playlistTitle>"
         f"<dc:queueItemId>{queue_item.queue_item_id}</dc:queueItemId>"
-        f"<upnp:albumArtURI>{image}</upnp:albumArtURI>"
+        f"<upnp:albumArtURI>{queue_item.image_url}</upnp:albumArtURI>"
         f"<upnp:class>{item_class}</upnp:class>"
         f"<upnp:mimeType>audio/{ext}</upnp:mimeType>"
         f'<res duration="{duration_str}" protocolInfo="http-get:*:audio/{ext}:DLNA.ORG_OP=00;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=0d500000000000000000000000000000">{url}</res>'
