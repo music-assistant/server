cb05e94d0d6d724e00fbb312a99b84d0ab1c72a5
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-03-19 21:23:29 +0100

    do not include self in versions

--- music_assistant/server/controllers/media/tracks.py
+++ music_assistant/server/controllers/media/tracks.py
@@ -134,15 +134,9 @@ class TracksController(MediaControllerBase[Track]):
             if loose_compare_strings(track.name, prov_item.name)
             and compare_artists(prov_item.artists, track.artists, any_match=True)
         }
-        # make sure that the 'base' version is included
+        # make sure that the 'base' version is NOT included
         for prov_version in track.provider_mappings:
-            if prov_version.item_id in all_versions:
-                continue
-            # grab full item here including album details etc
-            prov_track = await self.get_provider_item(
-                prov_version.item_id, prov_version.provider_instance
-            )
-            all_versions[prov_version.item_id] = prov_track
+            all_versions.pop(prov_version.item_id, None)
 
         # return the aggregated result
         return all_versions.values()
