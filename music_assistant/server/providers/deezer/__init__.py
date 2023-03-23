57c4b94db96333e0e60b5dae7915d420ef4dd8d4
Jonathan Bangert <jonathan@bangert.dk>
2023-03-23 22:12:19 +0100

    from requests to aiohttp and lots of other stuff

--- music_assistant/server/providers/deezer/__init__.py
+++ music_assistant/server/providers/deezer/__init__.py
@@ -1,25 +1,136 @@
-from music_assistant.common.models.enums import ProviderFeature
+"""Deezer musicprovider support for MusicAssistant."""
+from collections.abc import AsyncGenerator
+
+import deezer
+
+from music_assistant.common.models.enums import MediaType, ProviderFeature
+from music_assistant.common.models.media_items import Album, Artist, Playlist, SearchResults, Track
 from music_assistant.server.models.music_provider import MusicProvider
 
-from .helpers import *
+from .helpers import (
+    Credential,
+    get_album,
+    get_artist,
+    get_playlist,
+    get_track,
+    get_user_albums,
+    get_user_artists,
+    get_user_playlists,
+    get_user_tracks,
+    parse_album,
+    parse_artist,
+    parse_playlist,
+    parse_track,
+    search,
+)
+
+SUPPORTED_FEATURES = (
+    ProviderFeature.LIBRARY_ARTISTS,
+    ProviderFeature.LIBRARY_ALBUMS,
+    ProviderFeature.LIBRARY_TRACKS,
+    ProviderFeature.LIBRARY_PLAYLISTS,
+    ProviderFeature.LIBRARY_ARTISTS_EDIT,
+    ProviderFeature.LIBRARY_ALBUMS_EDIT,
+    ProviderFeature.LIBRARY_PLAYLISTS_EDIT,
+    ProviderFeature.LIBRARY_TRACKS_EDIT,
+    ProviderFeature.PLAYLIST_TRACKS_EDIT,
+    ProviderFeature.BROWSE,
+    ProviderFeature.SEARCH,
+    ProviderFeature.ARTIST_ALBUMS,
+    ProviderFeature.ARTIST_TOPTRACKS,
+)
 
 
 class DeezerProvider(MusicProvider):
-    """Deezer provider support"""
+    """Deezer provider support."""
+
+    creds: Credential
 
     async def setup(self) -> None:
         """Set up the Deezer provider."""
-        try:
-            creds = credential(
-                self.config.get_value("app_id"),
-                self.config.get_value("app_secret"),
-                self.config.get_value("access_token"),
-            )
-        except:
-            raise TypeError("whatever")
+        self.creds = Credential(
+            self.config.get_value("app_id"),  # type: ignore
+            self.config.get_value("app_secret"),  # type: ignore
+            self.config.get_value("access_token"),  # type: ignore
+        )
 
     @property
     def supported_features(self) -> tuple[ProviderFeature, ...]:
-        return (
-            ProviderFeature.SEARCH,
+        """Return the features supported by this Provider."""
+        return SUPPORTED_FEATURES
+
+    async def search(self, search_query: str, media_types=list[MediaType] | None) -> SearchResults:
+        """Perform search on musicprovider.
+
+        :param search_query: Search query.
+        :param media_types: A list of media_types to include. All types if None.
+        """
+        result = SearchResults()
+
+        filter = ""
+        if len(media_types) == 1:  # type: ignore
+            # Deezer does not support multiple searchtypes,
+            # falls back to track only if no type given
+            if media_types[0] == MediaType.ARTIST:  # type: ignore
+                filter = "artist"
+            elif media_types[0] == MediaType.ALBUM:  # type: ignore
+                filter = "album"
+            else:
+                filter = "track"
+        search_result = await search(query=search_query, filter=filter)
+        for thing in search_result:
+            if isinstance(thing, deezer.Track):
+                track = await parse_track(self, thing)
+                result.tracks.append(track)
+            elif isinstance(thing, deezer.Artist):
+                artist = await parse_artist(self, thing)
+                result.artists.append(artist)
+            elif isinstance(thing, deezer.Album):
+                album = await parse_album(self, thing)
+                result.albums.append(album)
+            else:
+                raise TypeError(
+                    "Something went wrong when searhing. Result was of type: {}".format(
+                        thing.type()
+                    )
+                )
+        return result
+
+    async def get_library_artists(self) -> AsyncGenerator[Artist, None]:
+        """Retrieve all library artists from Deezer."""
+        for artist in await get_user_artists(creds=self.creds):
+            yield await parse_artist(mass=self, artist=artist)
+
+    async def get_library_albums(self) -> AsyncGenerator[Album, None]:
+        """Retrieve all library albums from Deezer."""
+        for album in await get_user_albums(creds=self.creds):
+            yield await parse_album(mass=self, album=album)
+
+    async def get_library_playlists(self) -> AsyncGenerator[Playlist, None]:
+        """Retrieve all library playlists from Deezer."""
+        for playlist in await get_user_playlists(creds=self.creds):
+            yield await parse_playlist(mass=self, playlist=playlist)
+
+    async def get_library_tracks(self) -> AsyncGenerator[Track, None]:
+        """Retrieve all library tracks from Deezer."""
+        for track in await get_user_tracks(creds=self.creds):
+            yield await parse_track(mass=self, track=track)
+
+    async def get_artist(self, prov_artist_id: str) -> Artist:
+        """Get full artist details by id."""
+        return await parse_artist(mass=self, artist=await get_artist(artist_id=int(prov_artist_id)))
+
+    async def get_album(self, prov_album_id: str) -> Album:
+        """Get full album details by id."""
+        return await parse_album(mass=self, album=await get_album(album_id=int(prov_album_id)))
+
+    async def get_playlist(self, prov_playlist_id: str) -> Playlist:
+        """Get full playlist details by id."""
+        return await parse_playlist(
+            mass=self,
+            playlist=await get_playlist(creds=self.creds, playlist_id=int(prov_playlist_id)),
         )
+
+    async def get_track(self, prov_track_id: str) -> Track:
+        """Get full track details by id."""
+        return await parse_track(mass=self, track=await get_track(track_id=int(prov_track_id)))
--- music_assistant/server/providers/deezer/helpers.py
+++ music_assistant/server/providers/deezer/helpers.py
@@ -1,4 +1,4 @@
-"""Helper module for parsing the Deezer API. Also helper for getting audio streams
+"""Helper module for parsing the Deezer API. Also helper for getting audio streams.
 
 This helpers file is an async wrapper around the excellent deezer-python package.
 While the deezer-python package does an excellent job at parsing the Deezer results,
@@ -7,21 +7,40 @@ This also nicely separates the parsing logic from the Deezer provider logic.
 
 CREDITS:
 deezer-python: https://github.com/browniebroke/deezer-python by @browniebroke
-dzr: (which heavily inspired the track url and decoder but is not used) https://github.com/yne/dzr by @yne
+dzr: (inspired the track-url gatherer) https://github.com/yne/dzr by @yne.
 """
 
 import asyncio
 import json
 from time import time
 
+import aiohttp
 import deezer
-import requests
+
+from music_assistant.common.models.enums import (
+    AlbumType,
+    ContentType,
+    ImageType,
+    LinkType,
+    MediaType,
+)
+from music_assistant.common.models.media_items import (
+    Album,
+    Artist,
+    MediaItemImage,
+    MediaItemLink,
+    MediaItemMetadata,
+    Playlist,
+    ProviderMapping,
+    Track,
+)
 
 
-class credential:
-    """Class for storing credentials"""
+class Credential:
+    """Class for storing credentials."""
 
     def __init__(self, app_id: int, app_secret: str, access_token: str):
+        """Set the correct things."""
         self.app_id = app_id
         self.app_secret = app_secret
         self.access_token = access_token
@@ -31,17 +50,18 @@ class credential:
     access_token: str
 
 
-async def get_deezer_client(creds: credential = None) -> deezer.Client:  # type: ignore
+async def get_deezer_client(creds: Credential = None) -> deezer.Client:  # type: ignore
     """
-    Returns a deezer-python Client
-    If credentials are given the client is authorized. If no credentials are given the deezer client is not authorized
+    Return a deezer-python Client.
+
+    If credentials are given the client is authorized.
+    If no credentials are given the deezer client is not authorized.
 
     :param creds: Credentials. If none are given client is not authorized, defaults to None
     :type creds: credential, optional
     """
-    if creds:
-        if not isinstance(creds, credential):
-            raise TypeError("Creds must be of type credential")
+    if creds and not isinstance(creds, Credential):
+        raise TypeError("Creds must be of type credential")
 
     def _authorize():
         if creds:
@@ -56,8 +76,7 @@ async def get_deezer_client(creds: credential = None) -> deezer.Client:  # type:
 
 
 async def get_artist(artist_id: int) -> deezer.Artist:
-    """Async wrapper of the deezer-python get_artist function"""
-
+    """Async wrapper of the deezer-python get_artist function."""
     client = await get_deezer_client()
 
     def _get_artist():
@@ -68,8 +87,7 @@ async def get_artist(artist_id: int) -> deezer.Artist:
 
 
 async def get_album(album_id: int) -> deezer.Album:
-    """Async wrapper of the deezer-python get_album function"""
-
+    """Async wrapper of the deezer-python get_album function."""
     client = await get_deezer_client()
 
     def _get_album():
@@ -79,9 +97,8 @@ async def get_album(album_id: int) -> deezer.Album:
     return await asyncio.to_thread(_get_album)
 
 
-async def get_playlist(creds: credential, playlist_id) -> deezer.Playlist:
-    """Async wrapper of the deezer-python get_playlist function"""
-
+async def get_playlist(creds: Credential, playlist_id) -> deezer.Playlist:
+    """Async wrapper of the deezer-python get_playlist function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_playlist():
@@ -92,8 +109,7 @@ async def get_playlist(creds: credential, playlist_id) -> deezer.Playlist:
 
 
 async def get_track(track_id: int) -> deezer.Track:
-    """Async wrapper of the deezer-python get_track function"""
-
+    """Async wrapper of the deezer-python get_track function."""
     client = await get_deezer_client()
 
     def _get_track():
@@ -103,44 +119,52 @@ async def get_track(track_id: int) -> deezer.Track:
     return await asyncio.to_thread(_get_track)
 
 
-async def get_user_artists(creds: credential) -> deezer.PaginatedList:
-    """Async wrapper of the deezer-python get_user_artists function"""
-
+async def get_user_artists(creds: Credential) -> deezer.PaginatedList:
+    """Async wrapper of the deezer-python get_user_artists function."""
     client = await get_deezer_client(creds=creds)
 
-    def _get_track():
+    def _get_artist():
         artists = client.get_user_artists()
         return artists
 
-    return await asyncio.to_thread(_get_track)
+    return await asyncio.to_thread(_get_artist)
 
 
-# async def get_user_playlists(creds : credential) -> deezer.PaginatedList:
-#    """Async wrapper of the deezer-python get_user_playlists function"""
-#
-#    client = await get_deezer_client(creds=creds)
-#    def _get_track():
-#        playlists = client.get_user_playlists()
-#        return playlists
-#
-#    return await asyncio.to_thread(_get_track)
-
-
-async def get_user_albums(creds: credential) -> deezer.PaginatedList:
-    """Async wrapper of the deezer-python get_user_albums function"""
-
+async def get_user_playlists(creds: Credential) -> deezer.PaginatedList:
+    """Async wrapper of the deezer-python get_user_playlists function."""
     client = await get_deezer_client(creds=creds)
 
-    def _get_track():
+    def _get_playlist():
+        playlists = client.get_user().get_playlists()
+        return playlists
+
+    return await asyncio.to_thread(_get_playlist)
+
+
+async def get_user_albums(creds: Credential) -> deezer.PaginatedList:
+    """Async wrapper of the deezer-python get_user_albums function."""
+    client = await get_deezer_client(creds=creds)
+
+    def _get_album():
         albums = client.get_user_albums()
         return albums
 
+    return await asyncio.to_thread(_get_album)
+
+
+async def get_user_tracks(creds: Credential) -> deezer.PaginatedList:
+    """Async wrapper of the deezer-python get_user_tracks function."""
+    client = await get_deezer_client(creds=creds)
+
+    def _get_track():
+        tracks = client.get_user_tracks()
+        return tracks
+
     return await asyncio.to_thread(_get_track)
 
 
-async def add_user_albums(creds: credential, album_id: int) -> bool:
-    """Async wrapper of the deezer-python add_user_albums function"""
-
+async def add_user_albums(creds: Credential, album_id: int) -> bool:
+    """Async wrapper of the deezer-python add_user_albums function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_track():
@@ -150,9 +174,8 @@ async def add_user_albums(creds: credential, album_id: int) -> bool:
     return await asyncio.to_thread(_get_track)
 
 
-async def remove_user_albums(creds: credential, album_id: int) -> bool:
-    """Async wrapper of the deezer-python remove_user_albums function"""
-
+async def remove_user_albums(creds: Credential, album_id: int) -> bool:
+    """Async wrapper of the deezer-python remove_user_albums function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_track():
@@ -162,9 +185,8 @@ async def remove_user_albums(creds: credential, album_id: int) -> bool:
     return await asyncio.to_thread(_get_track)
 
 
-async def add_user_tracks(creds: credential, track_id: int) -> bool:
-    """Async wrapper of the deezer-python add_user_tracks function"""
-
+async def add_user_tracks(creds: Credential, track_id: int) -> bool:
+    """Async wrapper of the deezer-python add_user_tracks function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_track():
@@ -174,9 +196,8 @@ async def add_user_tracks(creds: credential, track_id: int) -> bool:
     return await asyncio.to_thread(_get_track)
 
 
-async def remove_user_tracks(creds: credential, track_id: int) -> bool:
-    """Async wrapper of the deezer-python remove_user_tracks function"""
-
+async def remove_user_tracks(creds: Credential, track_id: int) -> bool:
+    """Async wrapper of the deezer-python remove_user_tracks function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_track():
@@ -186,9 +207,8 @@ async def remove_user_tracks(creds: credential, track_id: int) -> bool:
     return await asyncio.to_thread(_get_track)
 
 
-async def add_user_artists(creds: credential, artist_id: int) -> bool:
-    """Async wrapper of the deezer-python add_user_artists function"""
-
+async def add_user_artists(creds: Credential, artist_id: int) -> bool:
+    """Async wrapper of the deezer-python add_user_artists function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_artist():
@@ -198,9 +218,8 @@ async def add_user_artists(creds: credential, artist_id: int) -> bool:
     return await asyncio.to_thread(_get_artist)
 
 
-async def remove_user_artists(creds: credential, artist_id: int) -> bool:
-    """Async wrapper of the deezer-python remove_user_artists function"""
-
+async def remove_user_artists(creds: Credential, artist_id: int) -> bool:
+    """Async wrapper of the deezer-python remove_user_artists function."""
     client = await get_deezer_client(creds=creds)
 
     def _get_artist():
@@ -211,8 +230,7 @@ async def remove_user_artists(creds: credential, artist_id: int) -> bool:
 
 
 async def search(query: str, filter: str = None) -> deezer.PaginatedList:  # type: ignore
-    """Async wrapper of the deezer-python search function"""
-
+    """Async wrapper of the deezer-python search function."""
     client = await get_deezer_client()
 
     def _search():
@@ -220,25 +238,33 @@ async def search(query: str, filter: str = None) -> deezer.PaginatedList:  # typ
             result = client.search_albums(query=query)
         elif filter == "artist":
             result = client.search_artists(query=query)
+        elif filter == "track":
+            result = client.search(query=query)
         else:
             result = client.search(query=query)
-        print(result)
         return result
 
     return await asyncio.to_thread(_search)
 
 
-async def _get_sid():
-    """Get a session id"""
-    return requests.get(
+async def _get_sid(mass):
+    """Get a session id."""
+    return await _get_http(
+        mass=mass,
         url="http://www.deezer.com/ajax/gw-light.php",
         params={"method": "deezer.ping", "api_version": "1.0", "api_token": ""},
-    ).json()["results"]["SESSION"]
+        headers=None,
+    )[  # type: ignore
+        "results"
+    ][
+        "SESSION"
+    ]
 
 
-async def _get_user_data(tok, sid):
+async def _get_user_data(mass, tok, sid):
     """Get user data."""
-    return requests.get(
+    return await _get_http(
+        mass=mass,
         url="https://www.deezer.com/ajax/gw-light.php",
         params={
             "method": "deezer.getUserData",
@@ -247,12 +273,15 @@ async def _get_user_data(tok, sid):
             "api_token": tok,
         },
         headers={"Cookie": f"sid={sid}"},
-    ).json()["results"]
+    )[  # type: ignore
+        "results"
+    ]
 
 
-async def _get_song_info(tok, sid, track_id):
-    """Get info for song. Cant use that of deezer-python because we need the track token"""
-    return requests.post(
+async def _get_song_info(mass, tok, sid, track_id):
+    """Get info for song. Can't use that of deezer-python because we need the track token."""
+    return await _post_http(
+        mass=mass,
         url="https://www.deezer.com/ajax/gw-light.php",
         params={
             "method": "song.getListData",
@@ -262,30 +291,214 @@ async def _get_song_info(tok, sid, track_id):
         },
         headers={"Cookie": f"sid={sid}"},
         data=json.dumps({"sng_ids": track_id}),
-    ).json()["results"]["data"][0]
+    )[  # type: ignore
+        "results"
+    ][
+        "data"
+    ][
+        0
+    ]
 
 
-async def _generate_url(usr_lic, track_tok):
-    """Get the url for the given track"""
+async def _generate_url(mass, usr_lic, track_tok):
+    """Get the url for the given track."""
     url = "https://media.deezer.com/v1/get_url"
     payload = {
         "license_token": usr_lic,
         "media": [{"type": "FULL", "formats": [{"cipher": "BF_CBC_STRIPE", "format": "MP3_128"}]}],
         "track_tokens": track_tok,
     }
-    response = requests.post(url, data=json.dumps(payload))
+    response = await _post_http(
+        mass=mass, url=url, data=json.dumps(payload), params=None, headers=None
+    )
     return response
 
 
-async def get_url(track_id) -> str:
-    sid = await _get_sid()
-    user_data = await _get_user_data("", sid)
+async def _get_http(mass, url, params, headers):
+    async with mass._throttler:
+        time_start = time.time()
+        try:
+            async with mass.mass.http_session.get(
+                url, headers=headers, params=params, verify_ssl=False, timeout=120
+            ) as response:
+                result = await response.json()
+                if "error" in result or ("status" in result and "error" in result["status"]):
+                    mass.logger.error("%s - %s", url, result)
+                    return None
+        except (
+            aiohttp.ContentTypeError,
+            json.JSONDecodeError,
+        ) as err:
+            mass.logger.error("%s - %s", url, str(err))
+            return None
+        finally:
+            mass.logger.debug(
+                "Processing GET/%s took %s seconds",
+                url,
+                round(time.time() - time_start, 2),
+            )
+        return result
+
+
+async def _post_http(mass, url, data, params, headers):
+    async with mass.mass.http_session.post(
+        url, headers=headers, params=params, json=data, verify_ssl=False
+    ) as response:
+        return await response.json()
+
+
+async def get_url(mass, track_id, creds: Credential) -> str:
+    """Get the url of the track."""
+    sid = await _get_sid(mass=mass)
+    user_data = await _get_user_data(mass=mass, tok=creds.access_token, sid=sid)
     licence_token = user_data["USER"]["OPTIONS"]["license_token"]
     check_form = user_data["checkForm"]
-    song_info = await _get_song_info(track_id=[track_id], sid=sid, tok=check_form)
+    song_info = await _get_song_info(mass=mass, track_id=[track_id], sid=sid, tok=check_form)
     track_token = song_info["TRACK_TOKEN"]
     track_id = song_info["SNG_ID"]
-    url_resp = await _generate_url(licence_token, [track_token])
-    url_info = url_resp.json()["data"][0]
+    url_resp = await _generate_url(mass, licence_token, [track_token])
+    url_info = url_resp["data"][0]  # type: ignore
     url = url_info["media"][0]["sources"][0]["url"]
     return url
+
+
+async def parse_artist(mass, artist: deezer.Artist) -> Artist:
+    """Parse the deezer-python artist to a MASS artist."""
+    if isinstance(artist, deezer.Track):
+        artst = Artist(
+            item_id=str(artist.id),
+            provider="deezer",
+            name=artist.title,
+            media_type=MediaType.ARTIST,
+            sort_name=artist.title_short,
+            provider_mappings={
+                ProviderMapping(
+                    item_id=str(artist.id),
+                    provider_domain="deezer",
+                    provider_instance=mass.instance_id,
+                    content_type=ContentType.MP3,
+                )
+            },
+            metadata=await parse_metadata_artist(artist=artist),
+        )
+        return artst
+    else:
+        raise TypeError("var track must be of type Track")
+
+
+async def parse_album_type(album_type: str) -> AlbumType:
+    """Parse the album type."""
+    type = AlbumType(
+        album_type,
+    )
+    return type
+
+
+async def parse_album(mass, album: deezer.Album) -> Album:
+    """Parse the deezer-python album to a MASS album."""
+    almb = Album(
+        album_type=await parse_album_type(album_type=album.type),
+        item_id=str(album.id),
+        provider="deezer",
+        name=album.title,
+        artists=[await parse_artist(mass=mass, artist=album.artist)],
+        media_type=MediaType.ALBUM,
+        provider_mappings={
+            ProviderMapping(
+                item_id=str(album.id),
+                provider_domain="deezer",
+                provider_instance=mass.instance_id,
+                content_type=ContentType.MP3,
+            )
+        },
+        metadata=await parse_metadata_album(album=album),
+    )
+    return almb
+
+
+async def parse_playlist(mass, playlist: deezer.Playlist) -> Playlist:
+    """Parse the deezer-python playlist to a MASS playlist."""
+    almb = Playlist(
+        item_id=str(playlist.id),
+        provider="deezer",
+        name=playlist.title,
+        media_type=MediaType.ALBUM,
+        provider_mappings={
+            ProviderMapping(
+                item_id=str(playlist.id),
+                provider_domain="deezer",
+                provider_instance=mass.instance_id,
+                content_type=ContentType.MP3,
+            )
+        },
+        metadata=await parse_metadata_playlist(playlist=playlist),
+    )
+    return almb
+
+
+async def parse_metadata_playlist(playlist: deezer.Playlist) -> MediaItemMetadata:
+    """Parse the playlist metadata."""
+    metadata = MediaItemMetadata(
+        links={MediaItemLink(type=LinkType.WEBSITE, url=playlist.link)},
+        preview=playlist.preview,
+        images=[MediaItemImage(type=ImageType.THUMB, url=playlist.picture_big, is_file=False)],
+    )
+    return metadata
+
+
+async def parse_metadata_track(track: deezer.Track) -> MediaItemMetadata:
+    """Parse the track metadata."""
+    metadata = MediaItemMetadata(
+        links={MediaItemLink(type=LinkType.WEBSITE, url=track.link)},
+        preview=track.preview,
+        images=[
+            MediaItemImage(type=ImageType.THUMB, url=track.get_album().cover_big, is_file=False)
+        ],
+    )
+    return metadata
+
+
+async def parse_metadata_album(album: deezer.Album) -> MediaItemMetadata:
+    """Parse the album metadata."""
+    metadata = MediaItemMetadata(
+        links={MediaItemLink(type=LinkType.WEBSITE, url=album.link)},
+        preview=album.preview,
+        images=[MediaItemImage(type=ImageType.THUMB, url=album.cover_big, is_file=False)],
+    )
+    return metadata
+
+
+async def parse_metadata_artist(artist: deezer.Artist) -> MediaItemMetadata:
+    """Parse the artist metadata."""
+    metadata = MediaItemMetadata(
+        links={MediaItemLink(type=LinkType.WEBSITE, url=artist.link)},
+        preview=artist.preview,
+        images=[MediaItemImage(type=ImageType.THUMB, url=artist.cover_big, is_file=False)],
+    )
+    return metadata
+
+
+async def parse_track(mass, track: deezer.Track) -> Track:
+    """Parse the deezer-python track to a MASS track."""
+    trk = Track(
+        item_id=str(track.id),
+        provider="deezer",
+        name=track.title,
+        media_type=MediaType.TRACK,
+        sort_name=track.title_short,
+        position=track.track_position,
+        duration=track.duration,
+        version=track.title_version,
+        artists=[await parse_artist(mass=mass, artist=track.artist)],
+        album=await parse_album(mass=mass, album=track.album),
+        provider_mappings={
+            ProviderMapping(
+                item_id=str(track.id),
+                provider_domain="deezer",
+                provider_instance=mass.instance_id,
+                content_type=ContentType.MP3,
+            )
+        },
+        metadata=await parse_metadata_track(track=track),
+    )
+    return trk
