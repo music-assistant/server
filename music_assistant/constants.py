812a833f1b4a48fa001619d537cd78dacac39ef1
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-03-27 09:31:01 +0200

    bump db schema

--- music_assistant/constants.py
+++ music_assistant/constants.py
@@ -5,7 +5,7 @@ from typing import Final
 
 __version__: Final[str] = "2.0.0b18"
 
-SCHEMA_VERSION: Final[int] = 21
+SCHEMA_VERSION: Final[int] = 22
 
 ROOT_LOGGER_NAME: Final[str] = "music_assistant"
 
