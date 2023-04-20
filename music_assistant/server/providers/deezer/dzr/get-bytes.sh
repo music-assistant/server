bca33bdba36f1c2717dfb54f5b5e00ce076680e2
Jonathan Bangert <jonathan@bangert.dk>
2023-04-20 09:22:59 +0200

    Remove dzr scripts

--- music_assistant/server/providers/deezer/dzr/get-bytes.sh
+++
@@ -1,6 +0,0 @@
-FETCH=${FETCH:-curl -s}
-unscramble(){ printf "${8}${16}${7}${15}${6}${14}${5}${13}${4}${12}${3}${11}${2}${10}${1}${9}";}
-APP_WEB=$($FETCH -L deezer.com/en/channels/explore | sed -n 's/.*src="\([^"]*app-web[^"]*\).*/\1/p' | xargs $FETCH -L)
-TMP_CBC=$(echo "$APP_WEB" | tr ,_ '\n' | sed -n 's/.*\(%5B0x[63]1%.\{48\}%5D\).*/\1/p' | sed 's/%5[BD]//g;s/%/\\x/g' | xargs -0 printf %b | sed 's/0x/\\x/g' | tr , '\n' | xargs -0 printf %b)
-export DZR_CBC=$(unscramble $TMP_CBC)
-./dzr-url.sh $1 | while read url id; do curl -s "$url" | ./dzr-dec.sh $id; done
