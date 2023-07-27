122e32fd0517b11fd9011b6346a21e016dd4a6d5
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-07-27 17:10:39 +0200

    prevent value changed on iteration error

--- music_assistant/server/providers/slimproto/__init__.py
+++ music_assistant/server/providers/slimproto/__init__.py
@@ -776,7 +776,7 @@ class SlimprotoProvider(PlayerProvider):
         # update all attributes
         await self._handle_player_update(client)
         # update existing players so they can update their `can_sync_with` field
-        for item in self._socket_clients.values():
+        for item in list(self._socket_clients.values()):
             if item.player_id == player_id:
                 continue
             await self._handle_player_update(item)
