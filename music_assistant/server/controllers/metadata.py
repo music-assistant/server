a66fc5ca167d2304c9408f39375efb99e3b9c6c7
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-08-05 23:47:51 +0200

    fix playlist image for non-local playlists

--- music_assistant/server/controllers/metadata.py
+++ music_assistant/server/controllers/metadata.py
@@ -205,8 +205,10 @@ class MetaDataController(CoreController):
             playlist.metadata.genres.update(playlist_genres_filtered)
 
             # create collage thumb/fanart from playlist tracks
-            if images:
+            # if playlist has no default image (e.g. a local playlist)
+            if images and (not playlist.image or playlist.image.provider != "url"):
                 if playlist.image and self.mass.storage_path in playlist.image.path:
+                    # re-use previous created path
                     img_path = playlist.image.path
                 else:
                     img_path = os.path.join(self.mass.storage_path, f"{uuid4().hex}.png")
