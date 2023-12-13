87fd426c6c0a5c320f88bb32ff04c2073d264df8
Santiago Soto <81064015+SantiagoSotoC@users.noreply.github.com>
2023-12-13 00:42:26 -0300

    Update music_assistant/server/providers/snapcast/__init__.py
    Co-authored-by: Marcel van der Veldt <m.vanderveldt@outlook.com>

--- music_assistant/server/providers/snapcast/__init__.py
+++ music_assistant/server/providers/snapcast/__init__.py
@@ -322,7 +322,7 @@ class SnapCastProvider(PlayerProvider):
 
     def _set_childs_state(self, player_id: str, state: PlayerState) -> None:
         """Set the state of the child`s of the player."""
-        for child in self._group_childs(player_id):
+        for child_player_id in self._group_childs(player_id):
             player = self.mass.players.get(child)
             player.state = state
             self.mass.players.update(player)
