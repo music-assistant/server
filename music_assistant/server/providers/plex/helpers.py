e2d4fa4f2158cd4b819455585cac8fac5aa27730
Jozef Kruszynski <jozef@krush.at>
2024-01-15 08:28:24 +0100

    Fixes for plex connection

--- music_assistant/server/providers/plex/helpers.py
+++ music_assistant/server/providers/plex/helpers.py
@@ -4,17 +4,17 @@ from __future__ import annotations
 import asyncio
 from typing import TYPE_CHECKING
 
-import plexapi.exceptions
 from plexapi.library import LibrarySection as PlexLibrarySection
 from plexapi.library import MusicSection as PlexMusicSection
-from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
 
 if TYPE_CHECKING:
     from music_assistant.server import MusicAssistant
 
 
-async def get_libraries(mass: MusicAssistant, auth_token: str) -> dict[str, PlexServer]:
+async def get_libraries(
+    mass: MusicAssistant, auth_token: str, local_server_ip: str, local_server_port: str
+) -> list[str]:
     """
     Get all music libraries for all plex servers.
 
@@ -24,23 +24,16 @@ async def get_libraries(mass: MusicAssistant, auth_token: str) -> dict[str, Plex
 
     def _get_libraries():
         # create a listing of available music libraries on all servers
-        all_libraries: dict[str, PlexServer] = {}
-        plex_account = MyPlexAccount(token=auth_token)
-        connected_servers = []
-        for resource in plex_account.resources():
-            if "server" not in resource.provides:
+        all_libraries: list[str] = []
+        plex_server: PlexServer = PlexServer(
+            f"http://{local_server_ip}:{local_server_port}", auth_token
+        )
+        for media_section in plex_server.library.sections():
+            media_section: PlexLibrarySection  # noqa: PLW2901
+            if media_section.type != PlexMusicSection.TYPE:
                 continue
-            try:
-                plex_server: PlexServer = resource.connect(None, 10)
-                connected_servers.append(plex_server)
-            except plexapi.exceptions.NotFound:
-                continue
-            for media_section in plex_server.library.sections():
-                media_section: PlexLibrarySection  # noqa: PLW2901
-                if media_section.type != PlexMusicSection.TYPE:
-                    continue
-                # TODO: figure out what plex uses as stable id and use that instead of names
-                all_libraries[f"{resource.name} / {media_section.title}"] = plex_server._baseurl
+            # TODO: figure out what plex uses as stable id and use that instead of names
+            all_libraries.append(f"{plex_server.friendlyName} / {media_section.title}")
         return all_libraries
 
     if cache := await mass.cache.get(cache_key, checksum=auth_token):
