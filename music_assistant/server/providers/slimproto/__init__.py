c3f2b0c0a9417499d0cf65bcfb629eabdfccfa94
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-03-12 19:51:52 +0100

    make cli ports static

--- music_assistant/server/providers/slimproto/__init__.py
+++ music_assistant/server/providers/slimproto/__init__.py
@@ -93,8 +93,8 @@ class SyncPlayPoint:
     diff: int
 
 
-CONF_CLI_TELNET = "cli_telnet"
-CONF_CLI_JSON = "cli_json"
+CONF_CLI_TELNET_PORT = "cli_telnet_port"
+CONF_CLI_JSON_PORT = "cli_json_port"
 CONF_DISCOVERY = "discovery"
 CONF_DISPLAY = "display"
 CONF_VISUALIZATION = "visualization"
@@ -155,14 +155,14 @@ async def get_config_entries(
     # ruff: noqa: ARG001
     return (
         ConfigEntry(
-            key=CONF_CLI_TELNET,
-            type=ConfigEntryType.BOOLEAN,
-            default_value=True,
-            label="Enable classic Squeezebox Telnet CLI",
+            key=CONF_CLI_TELNET_PORT,
+            type=ConfigEntryType.INTEGER,
+            default_value=9090,
+            label="Classic Squeezebox CLI Port",
             description="Some slimproto based players require the presence of the telnet CLI "
-            " to request more information. "
-            "By default this Telnet CLI is hosted on port 9090 but another port will be chosen if "
-            "that port is already taken. \n\n"
+            " to request more information. \n\n"
+            "By default this CLI is hosted on port 9090 but some players also accept "
+            "a different port. Set to 0 to disable this functionality.\n\n"
             "Commands allowed on this interface are very limited and just enough to satisfy "
             "player compatibility, so security risks are minimized to practically zero."
             "You may safely disable this option if you have no players that rely on this feature "
@@ -170,17 +170,17 @@ async def get_config_entries(
             advanced=True,
         ),
         ConfigEntry(
-            key=CONF_CLI_JSON,
-            type=ConfigEntryType.BOOLEAN,
-            default_value=True,
-            label="Enable JSON-RPC API",
+            key=CONF_CLI_JSON_PORT,
+            type=ConfigEntryType.INTEGER,
+            default_value=9000,
+            label="JSON-RPC CLI/API Port",
             description="Some slimproto based players require the presence of the JSON-RPC "
             "API from LMS to request more information. For example to fetch the album cover "
-            "and other metadata. "
+            "and other metadata. \n\n"
             "This JSON-RPC API is compatible with Logitech Media Server but not all commands "
             "are implemented. Just enough to satisfy player compatibility. \n\n"
-            "This API is hosted on the webserver responsible for streaming to players and thus "
-            "accessible on your local network but security impact should be minimal. "
+            "By default this JSON CLI is hosted on port 9000 but most players also accept "
+            "it on a different port. Set to 0 to disable this functionality.\n\n"
             "You may safely disable this option if you have no players that rely on this feature "
             "or you dont care about the additional metadata.",
             advanced=True,
@@ -228,12 +228,12 @@ class SlimprotoProvider(PlayerProvider):
         self._do_not_resync_before = {}
         self._resync_handle: asyncio.TimerHandle | None = None
         control_port = self.config.get_value(CONF_PORT)
-        enable_telnet = self.config.get_value(CONF_CLI_TELNET)
-        enable_json = self.config.get_value(CONF_CLI_JSON)
+        telnet_port = self.config.get_value(CONF_CLI_TELNET_PORT)
+        json_port = self.config.get_value(CONF_CLI_JSON_PORT)
         logging.getLogger("aioslimproto").setLevel(self.logger.level)
         self.slimproto = SlimServer(
-            cli_port=0 if enable_telnet else None,
-            cli_port_json=0 if enable_json else None,
+            cli_port=telnet_port or None,
+            cli_port_json=json_port or None,
             ip_address=self.mass.streams.publish_ip,
             name="Music Assistant",
             control_port=control_port,
