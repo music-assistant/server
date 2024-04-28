1ce8cdf4e9a2e4902a3392540f0a2999a830f538
MarvinSchenkel <marvin.schenkel@ah.nl>
2024-04-28 19:53:43 +0200

    Precommit

--- music_assistant/server/providers/apple_music/__init__.py
+++ music_assistant/server/providers/apple_music/__init__.py
@@ -2,31 +2,22 @@
 
 from __future__ import annotations
 
-import asyncio
 import base64
-import contextlib
 import json
 import os
-import platform
-import time
-from json.decoder import JSONDecodeError
-from tempfile import gettempdir
 from typing import TYPE_CHECKING, Any
 
 import aiofiles
 import m3u8
-from asyncio_throttle import Throttler
 from pywidevine import PSSH, Cdm, Device, DeviceTypes
 from pywidevine.license_protocol_pb2 import WidevinePsshData
 
 from music_assistant.common.helpers.json import json_loads
-from music_assistant.common.helpers.util import parse_title_and_version
 from music_assistant.common.models.config_entries import (ConfigEntry,
                                                           ConfigValueType)
 from music_assistant.common.models.enums import (ConfigEntryType, ExternalID,
                                                  ProviderFeature, StreamType)
-from music_assistant.common.models.errors import (LoginFailed,
-                                                  MediaNotFoundError)
+from music_assistant.common.models.errors import MediaNotFoundError
 from music_assistant.common.models.media_items import (Album, AlbumType,
                                                        Artist, AudioFormat,
                                                        ContentType, ImageType,
@@ -36,13 +27,7 @@ from music_assistant.common.models.media_items import (Album, AlbumType,
                                                        ProviderMapping,
                                                        SearchResults, Track)
 from music_assistant.common.models.streamdetails import StreamDetails
-from music_assistant.constants import CONF_PASSWORD, CONF_USERNAME
-# pylint: disable=no-name-in-module
-from music_assistant.server.helpers.app_vars import app_var
-# pylint: enable=no-name-in-module
-from music_assistant.server.helpers.audio import get_chunksize
-from music_assistant.server.helpers.playlists import parse_m3u
-from music_assistant.server.helpers.process import AsyncProcess, check_output
+from music_assistant.constants import CONF_PASSWORD
 from music_assistant.server.models.music_provider import MusicProvider
 
 if TYPE_CHECKING:
@@ -71,9 +56,10 @@ SUPPORTED_FEATURES = (
     # ProviderFeature.SIMILAR_TRACKS,
 )
 
-DEVELOPER_TOKEN = "Todo: parse from appvars"
+DEVELOPER_TOKEN = "TODO: Parse from appvars"
 DECRYPT_CLIENT_ID_FILENAME = "client_id.bin"
 DECRYPT_PRIVATE_KEY_FILENAME = "private_key.pem"
+
 
 async def setup(
     mass: MusicAssistant, manifest: ProviderManifest, config: ProviderConfig
@@ -121,9 +107,13 @@ class AppleMusicProvider(MusicProvider):
         self._music_user_token = self.config.get_value(CONF_PASSWORD)
         self._storefront = await self._get_user_storefront()
         base_path = os.path.join(os.path.dirname(__file__), "bin")
-        async with aiofiles.open(os.path.join(base_path, DECRYPT_CLIENT_ID_FILENAME), "rb") as _file:
+        async with aiofiles.open(
+            os.path.join(base_path, DECRYPT_CLIENT_ID_FILENAME), "rb"
+        ) as _file:
             self._decrypt_client_id = await _file.read()
-        async with aiofiles.open(os.path.join(base_path, DECRYPT_PRIVATE_KEY_FILENAME), "rb") as _file:
+        async with aiofiles.open(
+            os.path.join(base_path, DECRYPT_PRIVATE_KEY_FILENAME), "rb"
+        ) as _file:
             self._decrypt_private_key = await _file.read()
 
     @property
@@ -213,45 +203,33 @@ class AppleMusicProvider(MusicProvider):
         """Get all album tracks for given album id."""
         endpoint = f"catalog/{self._storefront}/albums/{prov_album_id}/tracks"
         response = await self._get_data(endpoint, include="artists")
-        return [
-          self._parse_track(track)
-          for track in response["data"]
-          if track["id"]
-        ]
+        return [self._parse_track(track) for track in response["data"] if track["id"]]
 
     async def get_playlist_tracks(self, prov_playlist_id) -> AsyncGenerator[Track, None]:
         """Get all playlist tracks for given playlist id."""
         if self._is_catalog_id(prov_playlist_id):
-          endpoint = f"catalog/{self._storefront}/playlists/{prov_playlist_id}/tracks"
+            endpoint = f"catalog/{self._storefront}/playlists/{prov_playlist_id}/tracks"
         else:
-          endpoint = f"me/library/playlists/{prov_playlist_id}/tracks"
+            endpoint = f"me/library/playlists/{prov_playlist_id}/tracks"
         count = 1
         for track in await self._get_all_items(endpoint, include="artists,catalog"):
-          if track and track["id"]:
-            track = self._parse_track(track)
-            track.position = count
-            yield track
-            count += 1
+            if track and track["id"]:
+                parsed_track = self._parse_track(track)
+                parsed_track.position = count
+                yield parsed_track
+                count += 1
 
     async def get_artist_albums(self, prov_artist_id) -> list[Album]:
         """Get a list of all albums for the given artist."""
         endpoint = f"catalog/{self._storefront}/artists/{prov_artist_id}/albums"
         response = await self._get_data(endpoint)
-        return [
-          self._parse_album(album)
-          for album in response["data"]
-          if album["id"]
-        ]
+        return [self._parse_album(album) for album in response["data"] if album["id"]]
 
     async def get_artist_toptracks(self, prov_artist_id) -> list[Track]:
         """Get a list of 10 most popular tracks for the given artist."""
         endpoint = f"catalog/{self._storefront}/artists/{prov_artist_id}/view/top-songs"
         response = await self._get_data(endpoint)
-        return [
-          self._parse_track(track)
-          for track in response["data"]
-          if track["id"]
-        ]
+        return [self._parse_track(track) for track in response["data"] if track["id"]]
 
     async def library_add(self, item: MediaItemType):
         """Add item to library."""
@@ -295,7 +273,7 @@ class AppleMusicProvider(MusicProvider):
     def _parse_artist(self, artist_obj):
         """Parse spotify artist object to generic layout."""
         relationships = artist_obj.get("relationships", {})
-        if artist_obj.get("type") == "library-artists" and relationships["catalog"]["data"] != []:          
+        if artist_obj.get("type") == "library-artists" and relationships["catalog"]["data"] != []:
             artist_id = relationships["catalog"]["data"][0]["id"]
             attributes = relationships["catalog"]["data"][0]["attributes"]
         elif "attributes" in artist_obj:
@@ -337,14 +315,14 @@ class AppleMusicProvider(MusicProvider):
         relationships = album_obj.get("relationships", {})
         response_type = album_obj.get("type")
         if response_type == "library-albums" and relationships["catalog"]["data"] != []:
-          album_id = relationships.get("catalog", {})["data"][0]["id"]
-          attributes = relationships.get("catalog", {})["data"][0]["attributes"]
+            album_id = relationships.get("catalog", {})["data"][0]["id"]
+            attributes = relationships.get("catalog", {})["data"][0]["attributes"]
         elif "attributes" in album_obj:
-          album_id = album_obj["id"]
-          attributes = album_obj["attributes"]
+            album_id = album_obj["id"]
+            attributes = album_obj["attributes"]
         else:
-          album_id = album_obj["id"]
-          attributes = {}
+            album_id = album_obj["id"]
+            attributes = {}
         album = Album(
             item_id=album_id,
             provider=self.domain,
@@ -375,8 +353,8 @@ class AppleMusicProvider(MusicProvider):
                     remotely_accessible=True,
                 )
             ]
-        if copyright := attributes.get("copyright"):
-            album.metadata.copyright = copyright
+        if album_copyright := attributes.get("copyright"):
+            album.metadata.copyright = album_copyright
         if record_label := attributes.get("recordLabel"):
             album.metadata.label = record_label
         if upc := attributes.get("upc"):
@@ -400,14 +378,14 @@ class AppleMusicProvider(MusicProvider):
         """Parse track object to generic layout."""
         relationships = track_obj.get("relationships", {})
         if track_obj.get("type") == "library-songs" and relationships["catalog"]["data"] != []:
-          track_id = relationships.get("catalog", {})["data"][0]["id"]
-          attributes = relationships.get("catalog", {})["data"][0]["attributes"]
+            track_id = relationships.get("catalog", {})["data"][0]["id"]
+            attributes = relationships.get("catalog", {})["data"][0]["attributes"]
         elif "attributes" in track_obj:
-          track_id = track_obj["id"]
-          attributes = track_obj["attributes"]
+            track_id = track_obj["id"]
+            attributes = track_obj["attributes"]
         else:
-          track_id = track_obj["id"]
-          attributes = {}
+            track_id = track_obj["id"]
+            attributes = {}
         track = Track(
             item_id=track_id,
             provider=self.domain,
@@ -418,9 +396,7 @@ class AppleMusicProvider(MusicProvider):
                     item_id=track_id,
                     provider_domain=self.domain,
                     provider_instance=self.instance_id,
-                    audio_format=AudioFormat(
-                        content_type=ContentType.AAC
-                    ),
+                    audio_format=AudioFormat(content_type=ContentType.AAC),
                     url=attributes.get("url"),
                     available=attributes.get("playParams", {}).get("id") is not None,
                 )
@@ -435,14 +411,14 @@ class AppleMusicProvider(MusicProvider):
         if albums := relationships.get("albums"):
             track.album = self._parse_album(albums["data"][0])
         if artwork := attributes.get("artwork"):
-          track.metadata.images = [
-              MediaItemImage(
-                  type=ImageType.THUMB,
-                  path=artwork["url"].format(w=artwork["width"], h=artwork["height"]),
-                  provider=self.instance_id,
-                  remotely_accessible=True,
-              )
-          ]
+            track.metadata.images = [
+                MediaItemImage(
+                    type=ImageType.THUMB,
+                    path=artwork["url"].format(w=artwork["width"], h=artwork["height"]),
+                    provider=self.instance_id,
+                    remotely_accessible=True,
+                )
+            ]
         if genres := attributes.get("genreNames"):
             track.metadata.genres = set(genres)
         if composers := attributes.get("composerName"):
@@ -483,7 +459,7 @@ class AppleMusicProvider(MusicProvider):
         if checksum := attributes.get("lastModifiedDate"):
             playlist.metadata.cache_checksum = checksum
         return playlist
-    
+
     async def _get_all_items(self, endpoint, key="data", **kwargs) -> list[dict]:
         """Get all items from a paged list."""
         limit = 50
@@ -504,18 +480,18 @@ class AppleMusicProvider(MusicProvider):
     async def _get_data(self, endpoint, **kwargs) -> dict[str, Any]:
         """Get data from api."""
         url = f"https://api.music.apple.com/v1/{endpoint}"
-        headers = {"Authorization": f'Bearer {DEVELOPER_TOKEN}'}
-        headers["Music-User-Token"] = self._music_user_token        
+        headers = {"Authorization": f"Bearer {DEVELOPER_TOKEN}"}
+        headers["Music-User-Token"] = self._music_user_token
         async with (
             self.mass.http_session.get(
                 url, headers=headers, params=kwargs, ssl=True, timeout=120
             ) as response,
         ):
-          # handle 404 not found, convert to MediaNotFoundError
-          if response.status == 404:
-              raise MediaNotFoundError(f"{endpoint} not found")
-          response.raise_for_status()
-          return await response.json(loads=json_loads)
+            # handle 404 not found, convert to MediaNotFoundError
+            if response.status == 404:
+                raise MediaNotFoundError(f"{endpoint} not found")
+            response.raise_for_status()
+            return await response.json(loads=json_loads)
 
     async def _delete_data(self, endpoint, data=None, **kwargs) -> str:
         """Delete data from api."""
@@ -528,31 +504,31 @@ class AppleMusicProvider(MusicProvider):
     async def _post_data(self, endpoint, data=None, **kwargs) -> str:
         """Post data on api."""
         raise NotImplementedError("Not implemented!")
-    
+
     async def _get_user_storefront(self) -> str:
         """Get the user's storefront."""
         locale = self.mass.metadata.locale.replace("_", "-")
         language = locale.split("-")[0]
         result = await self._get_data("me/storefront", l=language)
         return result["data"][0]["id"]
-    
-    def _is_catalog_id(self, input: str) -> bool:
+
+    def _is_catalog_id(self, catalog_id: str) -> bool:
         """Check if input is a catalog id, or a library id."""
-        return input.isnumeric()
-    
+        return catalog_id.isnumeric()
+
     async def _fetch_song_stream_metadata(self, song_id: str) -> str:
         """Get the stream URL for a song from Apple Music."""
         playback_url = "https://play.music.apple.com/WebObjects/MZPlay.woa/wa/webPlayback"
         data = {
             "salableAdamId": song_id,
-        }        
+        }
         async with self.mass.http_session.post(
             playback_url, headers=self._get_decryption_headers(), json=data, ssl=False
         ) as response:
             response.raise_for_status()
             content = await response.json(loads=json_loads)
             return content["songList"][0]
-        
+
     def _parse_stream_url_and_uri(self, stream_assets: list[dict]) -> str:
         """Parse the Stream URL and Key URI from the song."""
         ctrp256_urls = [asset["URL"] for asset in stream_assets if asset["flavor"] == "28:ctrp256"]
@@ -562,33 +538,44 @@ class AppleMusicProvider(MusicProvider):
         track_url = m3u8_playlist.base_uri + m3u8_playlist.segments[0].uri
         uri = m3u8_playlist.keys[0].uri
         return (track_url, uri)
-    
+
     def _get_decryption_headers(self):
-      """Get headers for decryption requests."""
-      return {
-          "authorization": f"Bearer {DEVELOPER_TOKEN}",
-          "media-user-token": self._music_user_token,
-          "connection": "keep-alive",
-          "accept": "application/json",
-          "origin": "https://music.apple.com",
-          "referer": "https://music.apple.com/",
-          "accept-encoding": "gzip, deflate, br",
-          "content-type": "application/json;charset=utf-8",
-          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+        """Get headers for decryption requests."""
+        return {
+            "authorization": f"Bearer {DEVELOPER_TOKEN}",
+            "media-user-token": self._music_user_token,
+            "connection": "keep-alive",
+            "accept": "application/json",
+            "origin": "https://music.apple.com",
+            "referer": "https://music.apple.com/",
+            "accept-encoding": "gzip, deflate, br",
+            "content-type": "application/json;charset=utf-8",
+            "user-agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                " Chrome/110.0.0.0 Safari/537.36"
+            ),
         }
-    
-    async def _get_decryption_key(self, license_url: str, key_id: str, uri: str, item_id: str) -> str:
+
+    async def _get_decryption_key(
+        self, license_url: str, key_id: str, uri: str, item_id: str
+    ) -> str:
         """Get the decryption key for a song."""
         pssh = self._get_pssh(key_id)
-        device = Device(client_id=self._decrypt_client_id, private_key=self._decrypt_private_key, type_=DeviceTypes.ANDROID, security_level=3, flags={})
+        device = Device(
+            client_id=self._decrypt_client_id,
+            private_key=self._decrypt_private_key,
+            type_=DeviceTypes.ANDROID,
+            security_level=3,
+            flags={},
+        )
         cdm = Cdm.from_device(device)
         session_id = cdm.open()
         challenge = cdm.get_license_challenge(session_id, pssh)
-        license = await self._get_license(challenge, license_url, uri, item_id)
-        cdm.parse_license(session_id, license)
+        track_license = await self._get_license(challenge, license_url, uri, item_id)
+        cdm.parse_license(session_id, track_license)
         key = [key for key in cdm.get_keys(session_id) if key.type == "CONTENT"][0]
         if not key:
-            raise MediaNotFoundError("Unable to get decryption key for song %s.".format(item_id))
+            raise MediaNotFoundError("Unable to get decryption key for song %s.".format())
         cdm.close(session_id)
         return key.key.hex()
 
@@ -598,11 +585,8 @@ class AppleMusicProvider(MusicProvider):
         pssh_data.algorithm = 1
         pssh_data.key_ids.append(key_id)
         init_data = base64.b64encode(pssh_data.SerializeToString()).decode("utf-8")
-        return PSSH.new(
-            system_id=PSSH.SystemId.Widevine,
-            init_data=init_data
-        )
-    
+        return PSSH.new(system_id=PSSH.SystemId.Widevine, init_data=init_data)
+
     async def _get_license(self, challenge: bytes, license_url: str, uri: str, item_id: str) -> str:
         """Get the license for a song based on the challenge."""
         challenge_b64 = base64.b64encode(challenge).decode("utf-8")
@@ -612,14 +596,14 @@ class AppleMusicProvider(MusicProvider):
             "uri": uri,
             "adamId": item_id,
             "isLibrary": False,
-            "user-initiated": True
+            "user-initiated": True,
         }
         async with self.mass.http_session.post(
             license_url, data=json.dumps(data), headers=self._get_decryption_headers(), ssl=False
         ) as response:
             response.raise_for_status()
             content = await response.json(loads=json_loads)
-            license = content.get("license")
-            if not license:
-                raise MediaNotFoundError("No license found for song %s.".format(item_id))
-            return license
+            track_license = content.get("license")
+            if not track_license:
+                raise MediaNotFoundError("No license found for song %s.".format())
+            return track_license
