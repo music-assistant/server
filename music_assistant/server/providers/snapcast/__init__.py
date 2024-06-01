a23d42b958481f05add67e01995a823aae7c9114
Santiago Soto <santiago@soto.uy>
2024-06-01 13:54:20 -0300

    Refactor SnapCastProvider cmd_volume_set method

--- music_assistant/server/providers/snapcast/__init__.py
+++ music_assistant/server/providers/snapcast/__init__.py
@@ -318,9 +318,7 @@ class SnapCastProvider(PlayerProvider):
     async def cmd_volume_set(self, player_id: str, volume_level: int) -> None:
         """Send VOLUME_SET command to given player."""
         snap_client_id = self._get_snapclient_id(player_id)
-        await self._snapserver.client_volume(
-            snap_client_id, {"percent": volume_level, "muted": volume_level == 0}
-        )
+        self._snapserver.client(snap_client_id).update_volume({"volume": {"percent": volume_level}})
 
     async def cmd_stop(self, player_id: str) -> None:
         """Send STOP command to given player."""
