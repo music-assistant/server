7f946574c35b56962c5bf32180d0869be133ee53
Santiago Soto <santiago@soto.uy>
2024-06-20 18:39:43 -0300

    Add description

--- music_assistant/server/providers/snapcast/__init__.py
+++ music_assistant/server/providers/snapcast/__init__.py
@@ -140,7 +140,10 @@ async def get_config_entries(
             type=ConfigEntryType.INTEGER,
             range=(500, 6000),
             default_value=1000,
-            label="Snapcast server buffer size",
+            label="Snapserver buffer size",
+            description="""Buffer[ms].The end-to-end latency,
+            from capturing a sample on the snapserver until
+            the sample is played-out on the client """,
             required=False,
             hidden=not snapserver_present,
             depends_on=not CONF_USE_EXTERNAL_SERVER,
