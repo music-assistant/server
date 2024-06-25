e58d7b08d873da4a14f00158845247e9ffe44593
John Carr <john.carr@unrouted.co.uk>
2024-06-25 16:17:36 +0100

    Better lint fix

--- music_assistant/server/providers/jellyfin/parsers.py
+++ music_assistant/server/providers/jellyfin/parsers.py
@@ -186,7 +186,7 @@ def parse_track(
 
     track.disc_number = jellyfin_track.get(ITEM_KEY_PARENT_INDEX_NUM, 0)
     track.track_number = jellyfin_track.get("IndexNumber", 0)
-    if track.track_number and track.track_number >= 0:
+    if track.track_number is not None and track.track_number >= 0:
         track.position = track.track_number
 
     track.metadata.images = _get_artwork(instance_id, client, jellyfin_track)
