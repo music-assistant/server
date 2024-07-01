dc5f224444381168666d9b6fdfa109955c88fc85
Santiago Soto <santiago@soto.uy>
2024-07-01 19:27:10 -0300

    Add dryout_ms parameter to snap_stream

--- music_assistant/server/providers/snapcast/__init__.py
+++ music_assistant/server/providers/snapcast/__init__.py
@@ -578,7 +578,7 @@ class SnapCastProvider(PlayerProvider):
             result = await self._snapserver.stream_add_stream(
                 # NOTE: setting the sampleformat to something else
                 # (like 24 bits bit depth) does not seem to work at all!
-                f"tcp://0.0.0.0:{port}?name={name}&sampleformat=48000:16:2",
+                f"tcp://0.0.0.0:{port}?name={name}&sampleformat=48000:16:2&dryout_ms=2000",
             )
             if "id" not in result:
                 # if the port is already taken, the result will be an error
