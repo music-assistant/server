f477619fa4dcda1388f541f993516f09682013a5
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-07-05 19:18:57 +0200

    add small TODO to airplay provider

--- music_assistant/server/providers/airplay/__init__.py
+++ music_assistant/server/providers/airplay/__init__.py
@@ -115,6 +115,15 @@ AIRPLAY_PCM_FORMAT = AudioFormat(
 CONF_ENTRY_SAMPLE_RATES_AIRPLAY = create_sample_rates_config_entry(44100, 16, 44100, 16, True)
 
 
+# TODO: Airplay provider
+# - split up and cleanup the code into more digestable parts
+# - Implement authentication for Apple TV
+# - Implement volume control for Apple devices using pyatv
+# - Implement metadata for Apple Apple devices using pyatv
+# - Use pyatv for communicating with original Apple devices
+# and use cliraop for actual streaming
+
+
 async def setup(
     mass: MusicAssistant, manifest: ProviderManifest, config: ProviderConfig
 ) -> ProviderInstanceType:
