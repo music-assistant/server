3703faea08881a97d5843ac7717ec0f07b1e03b6
Santiago Soto <santiago@soto.uy>
2024-07-11 18:36:28 -0300

    fix state in case player go offline

--- music_assistant/server/providers/snapcast/__init__.py
+++ music_assistant/server/providers/snapcast/__init__.py
@@ -371,6 +371,7 @@ class SnapCastProvider(PlayerProvider):
         mass_player.available = snap_client.connected
         mass_player.synced_to = self._synced_to(mass_player_id)
         if not snap_client.connected:
+            mass_player.state = PlayerState.IDLE
             asyncio.create_task(self.cmd_unsync(mass_player_id))
         if mass_player.active_group is None:
             if stream := self._get_snapstream(mass_player_id):
