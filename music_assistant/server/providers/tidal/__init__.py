85aa4e2cebd68e65fe4a23ae4bfc22338f730636
Jozef Kruszynski <jozef+github@krush.at>
2024-07-21 16:03:27 +0200

    some things

--- music_assistant/server/providers/tidal/__init__.py
+++ music_assistant/server/providers/tidal/__init__.py
@@ -507,10 +507,8 @@ class TidalProvider(MusicProvider):
         # TODO - This is all wrong and needs to be fixed
         stream: TidalStream = await get_stream(track)
         manifest = stream.get_stream_manifest()
-        hls = manifest.get_hls()
-
         # url = await get_track_url(tidal_session, item_id)
-        # media_info = await self._get_media_info(item_id=item_id, url=url)
+        # media_info = await self._get_media_info(item_idis=item_id, url=url)
         if not track:
             msg = f"track {item_id} not found"
             raise MediaNotFoundError(msg)
@@ -523,9 +521,9 @@ class TidalProvider(MusicProvider):
                 bit_depth=stream.bit_depth,
                 channels=2,
             ),
-            stream_type=StreamType.HLS,
+            stream_type=StreamType.HLS if stream.is_MPD else StreamType.HTTP,
             duration=track.duration,
-            path=hls,
+            path=manifest.get_urls(),
         )
 
     @throttle_with_retries
