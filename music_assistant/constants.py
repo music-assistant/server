9313a0d276e1dbf0937aa0d457a9a91968b52966
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-08-24 23:50:39 +0200

    Add config option to specify the http content length header (#1607)

--- music_assistant/constants.py
+++ music_assistant/constants.py
@@ -65,6 +65,7 @@ CONF_ANNOUNCE_VOLUME_MAX: Final[str] = "announce_volume_max"
 CONF_ICON: Final[str] = "icon"
 CONF_LANGUAGE: Final[str] = "language"
 CONF_SAMPLE_RATES: Final[str] = "sample_rates"
+CONF_HTTP_PROFILE: Final[str] = "http_profile"
 
 # config default values
 DEFAULT_HOST: Final[str] = "0.0.0.0"
