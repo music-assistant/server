dd0b8b301b1dc46fe33b50901b81fcc85319e447
Marvin Schenkel <marvinschenkel@gmail.com>
2024-09-05 11:39:48 +0200

    Fix unavailable albums from showing up in MA. Fix not all artist albums getting pulled.

--- music_assistant/server/providers/apple_music/__init__.py
+++ music_assistant/server/providers/apple_music/__init__.py
@@ -191,7 +191,9 @@ class AppleMusicProvider(MusicProvider):
             endpoint, include="catalog,artists", extend="editorialNotes"
         ):
             if item and item["id"]:
-                yield self._parse_album(item)
+                album = self._parse_album(item)
+                if album:
+                    yield album
 
     async def get_library_tracks(self) -> AsyncGenerator[Track, None]:
         """Retrieve library tracks from the provider."""
@@ -296,12 +298,12 @@ class AppleMusicProvider(MusicProvider):
         """Get a list of all albums for the given artist."""
         endpoint = f"catalog/{self._storefront}/artists/{prov_artist_id}/albums"
         try:
-            response = await self._get_data(endpoint)
+            response = await self._get_all_items(endpoint)
         except MediaNotFoundError:
             # Some artists do not have albums, return empty list
             self.logger.info("No albums found for artist %s", prov_artist_id)
             return []
-        return [self._parse_album(album) for album in response["data"] if album["id"]]
+        return [self._parse_album(album) for album in response if album["id"]]
 
     async def get_artist_toptracks(self, prov_artist_id) -> list[Track]:
         """Get a list of 10 most popular tracks for the given artist."""
@@ -418,7 +420,7 @@ class AppleMusicProvider(MusicProvider):
             artist.metadata.description = notes.get("standard") or notes.get("short")
         return artist
 
-    def _parse_album(self, album_obj: dict) -> Album | ItemMapping:
+    def _parse_album(self, album_obj: dict) -> Album | ItemMapping | None:
         """Parse album object to generic layout."""
         relationships = album_obj.get("relationships", {})
         response_type = album_obj.get("type")
@@ -441,6 +443,13 @@ class AppleMusicProvider(MusicProvider):
                 item_id=album_id,
                 name=album_id,
             )
+        is_available_in_catalog = attributes.get("url") is not None
+        if not is_available_in_catalog:
+            self.logger.debug(
+                "Skipping album %s. Album is not available in the Apple Music catalog.",
+                attributes.get("name"),
+            )
+            return None
         album = Album(
             item_id=album_id,
             provider=self.domain,
