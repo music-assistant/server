1ff831e05e53f1b44458e37db10b38a3ce6faa61
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-09-14 03:06:08 +0200

    handle race condition in sonos

--- music_assistant/server/providers/sonos/__init__.py
+++ music_assistant/server/providers/sonos/__init__.py
@@ -330,7 +330,7 @@ class SonosPlayer:
         else:
             # player is group child (synced to another player)
             group_parent = self.prov.sonos_players.get(self.client.player.group.coordinator_id)
-            if not group_parent:
+            if not group_parent or not group_parent.client:
                 # handle race condition where the group parent is not yet discovered
                 return
             active_group = group_parent.client.player.group
