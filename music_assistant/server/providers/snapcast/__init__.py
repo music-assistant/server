4c85792e41c430fac4a0aa4277f6937c0cfd6412
Santiago Soto <santiago@soto.uy>
2024-10-01 17:27:50 -0300

    set idle_threshold to 60000

--- music_assistant/server/providers/snapcast/__init__.py
+++ music_assistant/server/providers/snapcast/__init__.py
@@ -588,7 +588,7 @@ class SnapCastProvider(PlayerProvider):
             result = await self._snapserver.stream_add_stream(
                 # NOTE: setting the sampleformat to something else
                 # (like 24 bits bit depth) does not seem to work at all!
-                f"tcp://0.0.0.0:{port}?name={name}&sampleformat=48000:16:2",
+                f"tcp://0.0.0.0:{port}?name={name}&sampleformat=48000:16:2&idle_threshold=60000",
             )
             if "id" not in result:
                 # if the port is already taken, the result will be an error
