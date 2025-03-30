e90232c1e525141644fba0eb775178424a872eb5
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-03-30 15:24:27 +0200

    Chore: some tweaks to DIDL metadata

--- music_assistant/helpers/didl_lite.py
+++ music_assistant/helpers/didl_lite.py
@@ -30,22 +30,23 @@ def create_didl_metadata(media: PlayerMedia) -> str:
             f"<dc:queueItemId>{media.uri}</dc:queueItemId>"
             "<upnp:class>object.item.audioItem.audioBroadcast</upnp:class>"
             f"<upnp:mimeType>audio/{ext}</upnp:mimeType>"
-            f'<res duration="23:59:59.000" protocolInfo="http-get:*:audio/{ext}:DLNA.ORG_PN={ext.upper()};DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=0d500000000000000000000000000000">{escape_string(media.uri)}</res>'
+            f'<res protocolInfo="http-get:*:audio/{ext}:DLNA.ORG_PN={ext.upper()};DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=0d500000000000000000000000000000">{escape_string(media.uri)}</res>'
             "</item>"
             "</DIDL-Lite>"
         )
     duration_str = str(datetime.timedelta(seconds=media.duration or 0)) + ".000"
     return (
-        '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/">'
-        '<item id="1" parentID="0" restricted="1">'
+        '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/">'
+        f'<item id="{media.queue_item_id or media.uri}" restricted="true">'
         f"<dc:title>{escape_string(media.title or media.uri)}</dc:title>"
         f"<dc:creator>{escape_string(media.artist or '')}</dc:creator>"
         f"<upnp:album>{escape_string(media.album or '')}</upnp:album>"
         f"<upnp:artist>{escape_string(media.artist or '')}</upnp:artist>"
         f"<upnp:duration>{int(media.duration or 0)}</upnp:duration>"
         f"<dc:queueItemId>{media.uri}</dc:queueItemId>"
+        f"<dc:description>Music Assistant</dc:description>"
         f"<upnp:albumArtURI>{escape_string(image_url)}</upnp:albumArtURI>"
-        "<upnp:class>object.item.audioItem.audioBroadcast</upnp:class>"
+        "<upnp:class>object.item.audioItem.musicTrack</upnp:class>"
         f"<upnp:mimeType>audio/{ext}</upnp:mimeType>"
         f'<res duration="{duration_str}" protocolInfo="http-get:*:audio/{ext}:DLNA.ORG_PN={ext.upper()};DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=0d500000000000000000000000000000">{escape_string(media.uri)}</res>'
         "</item>"
