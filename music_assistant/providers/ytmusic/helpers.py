a2eccd6c180dc2ffe9d1d3101683d883902eb13a
Marvin Schenkel <marvin.schenkel@nn-group.com>
2025-04-01 16:14:33 +0200

    Fix missing track thumbs

--- music_assistant/providers/ytmusic/helpers.py
+++ music_assistant/providers/ytmusic/helpers.py
@@ -86,7 +86,7 @@ async def get_track(
             "thumbnails"
         ]
         if track_thumbs := track_obj["videoDetails"].get("thumbnail", {}).get("thumbnails"):
-            track["thumbnails"] = track["thumbnails"] + track_thumbs
+            track["thumbnails"] = track.get("thumbnails", []) + track_thumbs
         track["isAvailable"] = track_obj["playabilityStatus"]["status"] == "OK"
         return track
 
