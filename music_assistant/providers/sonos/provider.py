5c6dcc8f29d764f6b00fd972054050a0ec33c385
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-04-05 15:26:56 +0200

    hide flow mode for sonos again

--- music_assistant/providers/sonos/provider.py
+++ music_assistant/providers/sonos/provider.py
@@ -22,6 +22,7 @@ from music_assistant_models.player import DeviceInfo, PlayerMedia
 from zeroconf import ServiceStateChange
 
 from music_assistant.constants import (
+    CONF_ENTRY_FLOW_MODE_HIDDEN_DISABLED,
     CONF_ENTRY_HTTP_PROFILE_DEFAULT_2,
     CONF_ENTRY_MANUAL_DISCOVERY_IPS,
     CONF_ENTRY_OUTPUT_CODEC,
