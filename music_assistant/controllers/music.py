66a9fbab50aed5589e6c34e0a1cb01e481bcb75a
OzGav <gavnosp@hotmail.com>
2025-06-10 22:17:09 +0100

    Fix typo preventing translation (#2219)

--- music_assistant/controllers/music.py
+++ music_assistant/controllers/music.py
@@ -1194,7 +1194,7 @@ class MusicController(CoreController):
                 item_id="favorite_radio",
                 provider="library",
                 name="Favorite Radio stations",
-                translation_key="favorite_radio",
+                translation_key="favorite_radio_stations",
                 icon="mdi-access-point",
                 items=await self.radio.library_items(favorite=True, limit=10, order_by="random"),
             ),
