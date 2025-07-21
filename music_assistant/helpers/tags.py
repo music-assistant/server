f122390e1492b7be564c8011785ca15762293f55
OzGav <gavnosp@hotmail.com>
2025-07-22 00:17:17 +1000

    Update music_assistant/helpers/tags.py
    Co-authored-by: Maxim Raznatovski <nda.mr43@gmail.com>

--- music_assistant/helpers/tags.py
+++ music_assistant/helpers/tags.py
@@ -405,8 +405,8 @@ class AudioTags:
         """Try to read/calculate the integrated loudness from the tags (album level)."""
         if tag := self.tags.get("r128albumgain"):
             try:
-                value = int(tag.split(" ")[0]) / 256
-                return -23 - float(value)
+                gain_adjustment = int(tag.split(" ")[0]) / 256
+                return -23 - gain_adjustment
             except (ValueError, IndexError) as e:
                 LOGGER.warning(f"Invalid r128albumgain tag value: {tag!r} — {e}")
 
