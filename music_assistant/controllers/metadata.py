ecff69c9d049d72da2fb89d9dac6977006818db7
OzGav <gavnosp@hotmail.com>
2025-08-16 05:51:10 +1000

    Update music_assistant/controllers/metadata.py
    Co-authored-by: Marcel van der Veldt <m.vanderveldt@outlook.com>

--- music_assistant/controllers/metadata.py
+++ music_assistant/controllers/metadata.py
@@ -316,8 +316,7 @@ class MetaDataController(CoreController):
             if media_item.image and media_item.image.type == img_type:
                 if not media_item.image.remotely_accessible and resolve:
                     return self.get_image_url(media_item.image)
-                else:
-                    return media_item.image.path
+                return media_item.image.path
 
             # Only retrieve full item if we don't have the image we need
             if not media_item.uri:
