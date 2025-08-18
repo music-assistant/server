906fb0dddad28508a14a401206768ada397650dc
Shi-553 <58556078+Shi-553@users.noreply.github>
2025-08-18 06:03:34 +0000

    Fix: Refactor configuration constants for nicovideo provider by moving keys to config.py and cleaning up imports

--- music_assistant/providers/nicovideo/config.py
+++ music_assistant/providers/nicovideo/config.py
@@ -9,31 +9,33 @@ from music_assistant_models.config_entries import (
 )
 from music_assistant_models.enums import ConfigEntryType
 
-from music_assistant.constants import CONF_PASSWORD
 from music_assistant.controllers.config import ConfigController
-from music_assistant.providers.nicovideo.constants import (
-    CONF_AUTO_LIKE_ON_LIBRARY_ADD,
-    CONF_FOLLOWING_ACTIVITIES_COUNT,
-    CONF_HISTORY_COUNT,
-    CONF_INCLUDE_FOLLOWED_MYLISTS,
-    CONF_INCLUDE_FOLLOWED_MYLISTS_TRACKS,
-    CONF_INCLUDE_LIBRARY_TRACK_ARTISTS,
-    CONF_INCLUDE_OWN_MYLISTS_TRACKS,
-    CONF_INCLUDE_OWN_SERIES_ALBUMS,
-    CONF_INCLUDE_OWN_VIDEOS_TRACKS,
-    CONF_MAIL,
-    CONF_MFA,
-    CONF_RECOMMENDATION_COUNT,
-    CONF_RECOMMENDATION_FILTER_TAGS,
-    CONF_TAG_RECOMMENDATION_NEW_TRACKS_TAGS,
-    CONF_TAG_RECOMMENDATION_TAGS,
-    CONF_USER_SESSION,
-)
 
 if TYPE_CHECKING:
     from music_assistant_models.config_entries import ConfigValueType, ProviderConfig
 
     from music_assistant.models.provider import Provider
+
+
+# Configuration keys for nicovideo provider settings
+CONF_MAIL = "mail"
+CONF_PASSWORD = "password"
+CONF_MFA = "mfa"
+CONF_USER_SESSION = "user_session"
+CONF_AUTO_LIKE_ON_LIBRARY_ADD = "auto_like_on_library_add"
+CONF_USE_FOLLOW_UNFOLLOW_ARTISTS = "use_follow_unfollow_artists"
+CONF_INCLUDE_FOLLOWED_MYLISTS = "include_followed_mylists"
+CONF_INCLUDE_FOLLOWED_MYLISTS_TRACKS = "include_followed_mylists_tracks"
+CONF_INCLUDE_OWN_SERIES_ALBUMS = "include_own_series_albums"
+CONF_INCLUDE_OWN_VIDEOS_TRACKS = "include_own_videos_tracks"
+CONF_INCLUDE_OWN_MYLISTS_TRACKS = "include_own_mylists_tracks"
+CONF_INCLUDE_LIBRARY_TRACK_ARTISTS = "include_library_track_artists"
+CONF_RECOMMENDATION_COUNT = "recommendation_count"
+CONF_RECOMMENDATION_FILTER_TAGS = "recommendation_filter_tags"
+CONF_TAG_RECOMMENDATION_TAGS = "tag_recommendation_tags"
+CONF_TAG_RECOMMENDATION_NEW_TRACKS_TAGS = "tag_recommendation_new_tracks_tags"
+CONF_HISTORY_COUNT = "history_count"
+CONF_FOLLOWING_ACTIVITIES_COUNT = "following_activities_count"
 
 
 class AuthCredentials:
--- music_assistant/providers/nicovideo/constants.py
+++ music_assistant/providers/nicovideo/constants.py
@@ -14,25 +14,6 @@ class ApiPriority(Enum):
     LOW = "low"
 
 
-# Configuration keys for nicovideo provider settings
-CONF_MAIL = "mail"
-CONF_MFA = "mfa"
-CONF_USER_SESSION = "user_session"
-CONF_AUTO_LIKE_ON_LIBRARY_ADD = "auto_like_on_library_add"
-CONF_USE_FOLLOW_UNFOLLOW_ARTISTS = "use_follow_unfollow_artists"
-CONF_INCLUDE_FOLLOWED_MYLISTS = "include_followed_mylists"
-CONF_INCLUDE_FOLLOWED_MYLISTS_TRACKS = "include_followed_mylists_tracks"
-CONF_INCLUDE_OWN_SERIES_ALBUMS = "include_own_series_albums"
-CONF_INCLUDE_OWN_VIDEOS_TRACKS = "include_own_videos_tracks"
-CONF_INCLUDE_OWN_MYLISTS_TRACKS = "include_own_mylists_tracks"
-CONF_INCLUDE_LIBRARY_TRACK_ARTISTS = "include_library_track_artists"
-CONF_RECOMMENDATION_COUNT = "recommendation_count"
-CONF_RECOMMENDATION_FILTER_TAGS = "recommendation_filter_tags"
-CONF_TAG_RECOMMENDATION_TAGS = "tag_recommendation_tags"
-CONF_TAG_RECOMMENDATION_NEW_TRACKS_TAGS = "tag_recommendation_new_tracks_tags"
-CONF_HISTORY_COUNT = "history_count"
-CONF_FOLLOWING_ACTIVITIES_COUNT = "following_activities_count"
-
 # Network constants
 NICOVIDEO_USER_AGENT = "Music Assistant/1.0"
 
