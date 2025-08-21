9bcc8f935cc46781d654ccac2201e28303f39623
Gav <gavnosp@hotmail.com>
2025-08-21 22:27:08 +1000

    Fix constants.py

--- music_assistant/providers/spotify/constants.py
+++ music_assistant/providers/spotify/constants.py
@@ -43,6 +43,24 @@ CALLBACK_REDIRECT_URL = "https://music-assistant.io/callback"
 # Other Constants
 LIKED_SONGS_FAKE_PLAYLIST_ID_PREFIX = "liked_songs"
 
+# Base Features
+SUPPORTED_FEATURES = {
+    ProviderFeature.LIBRARY_ARTISTS,
+    ProviderFeature.LIBRARY_ALBUMS,
+    ProviderFeature.LIBRARY_TRACKS,
+    ProviderFeature.LIBRARY_PLAYLISTS,
+    ProviderFeature.LIBRARY_ARTISTS_EDIT,
+    ProviderFeature.LIBRARY_ALBUMS_EDIT,
+    ProviderFeature.LIBRARY_PLAYLISTS_EDIT,
+    ProviderFeature.LIBRARY_TRACKS_EDIT,
+    ProviderFeature.PLAYLIST_TRACKS_EDIT,
+    ProviderFeature.BROWSE,
+    ProviderFeature.SEARCH,
+    ProviderFeature.ARTIST_ALBUMS,
+    ProviderFeature.ARTIST_TOPTRACKS,
+    ProviderFeature.SIMILAR_TRACKS,
+}
+
 # Podcast Features
 PODCAST_FEATURES = {
     ProviderFeature.LIBRARY_PODCASTS,
