da3951869ffe08c006ae84e563085a0aebe4d72e
Maxim Raznatovski <nda.mr43@gmail.com>
2025-09-09 11:25:54 +0200

    fix: remove unregistering of players on load

--- music_assistant/providers/hass_players/provider.py
+++ music_assistant/providers/hass_players/provider.py
@@ -62,10 +62,6 @@ class HomeAssistantPlayerProvider(PlayerProvider):
         self.on_unload_callbacks = [
             await self.hass_prov.hass.subscribe_entities(self._on_entity_state_update, player_ids)
         ]
-        # remove any leftover players (after reconfigure of players)
-        for player in self.players:
-            if player.player_id not in player_ids:
-                await self.mass.players.unregister(player.player_id)
 
     async def unload(self, is_removed: bool = False) -> None:
         """
