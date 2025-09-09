ec629748c8620bb1d51ce869f0b54d5d81dd4fb8
OzGav <gavnosp@hotmail.com>
2025-09-09 23:46:05 +1000

    Remove unneeded constants

--- music_assistant/providers/podcast-index/constants.py
+++ music_assistant/providers/podcast-index/constants.py
@@ -8,11 +8,6 @@ CONF_STORED_PODCASTS = "stored_podcasts"
 # API settings
 API_BASE_URL = "https://api.podcastindex.org/api/1.0"
 
-# Cache categories - following MA standard naming convention
-CACHE_PODCASTS = 900
-CACHE_EPISODES = 901
-CACHE_CATEGORIES = 902
-
 # Browse categories
 BROWSE_TRENDING = "trending"
 BROWSE_RECENT = "recent"
