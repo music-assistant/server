f64b1f2c965dc982133538483adc94c6cc9fcd2f
Maxim Raznatovski <nda.mr43@gmail.com>
2025-09-15 09:43:50 +0200

    refactor(SyncGroupPlayer): inline `_set_attributes()`

--- music_assistant/models/player.py
+++ music_assistant/models/player.py
@@ -1377,6 +1377,10 @@ class SyncGroupPlayer(GroupPlayer):
         self._attr_powered = False  # group players are always powered off by default
         self._attr_active_source = player_id
         self._attr_device_info = DeviceInfo(model="Sync Group", manufacturer=provider.name)
+        self._attr_supported_features = {
+            PlayerFeature.POWER,
+            PlayerFeature.VOLUME_SET,
+        }
 
     async def on_registered(self) -> None:
         """Complete the initialization once the player was registered."""
@@ -1385,7 +1389,9 @@ class SyncGroupPlayer(GroupPlayer):
         self._attr_group_members = list(
             cast("list[str]", self.config.get_value(CONF_GROUP_MEMBERS, []))
         )
-        self._set_attributes()  # uses self.config
+        # Uses self.config
+        if self.is_dynamic:
+            self._attr_supported_features.add(PlayerFeature.SET_MEMBERS)
 
     @property
     def supported_features(self) -> set[PlayerFeature]:
@@ -1444,17 +1450,6 @@ class SyncGroupPlayer(GroupPlayer):
             return {self.provider.lookup_key}
         else:
             return set()
-
-    def _set_attributes(self) -> None:
-        """Set player attributes."""
-        player_features = {
-            PlayerFeature.POWER,
-            PlayerFeature.VOLUME_SET,
-        }
-        if self.is_dynamic:
-            player_features.add(PlayerFeature.SET_MEMBERS)
-
-        self._attr_supported_features = player_features
 
     async def get_config_entries(self) -> list[ConfigEntry]:
         """Return all (provider/player specific) Config Entries for the given player (if any)."""
