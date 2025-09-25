6d76e1474d3190db912a32b67896f880517d7fcc
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-09-25 18:42:48 +0200

    Update __init__.py

--- music_assistant/providers/ytmusic/__init__.py
+++ music_assistant/providers/ytmusic/__init__.py
@@ -135,7 +135,7 @@ SUPPORTED_FEATURES = {
 
 
 # TODO: fix disabled tests
-# ruff: noqa: PLW2901
+# ruff: noqa: PLW2901, RET504
 
 
 async def setup(
@@ -934,6 +934,9 @@ class YoutubeMusicProvider(MusicProvider):
                 "extractor_args": {
                     "youtubepot-bgutilhttp": {
                         "base_url": [self._po_token_server_url],
+                        # Disable new PO Token server behavior. Disable after this issue is fixed:
+                        # https://github.com/Brainicism/bgutil-ytdlp-pot-provider/issues/138
+                        "disable_innertube": "1",
                     },
                     "youtube": {
                         "skip": ["translated_subs", "dash"],
