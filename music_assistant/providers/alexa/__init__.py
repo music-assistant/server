bea39a1c1ebe3a8af96262c9dbded1557f1b3168
Foakx <chris@industed.com>
2025-09-28 14:46:27 +0200

    Fix linting

--- music_assistant/providers/alexa/__init__.py
+++ music_assistant/providers/alexa/__init__.py
@@ -22,8 +22,8 @@ from music_assistant_models.errors import LoginFailed
 from music_assistant_models.player import DeviceInfo, PlayerMedia
 
 from music_assistant.constants import (
-    CONF_ENTRY_DEPRECATED_CROSSFADE,
     CONF_ENTRY_CROSSFADE_DURATION,
+    CONF_ENTRY_DEPRECATED_CROSSFADE,
     CONF_ENTRY_FLOW_MODE_ENFORCED,
     CONF_ENTRY_HTTP_PROFILE,
     CONF_PASSWORD,
