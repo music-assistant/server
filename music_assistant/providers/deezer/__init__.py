7d3aa2e056e1d3b6707ef31cf883bf4e27f7dba8
OzGav <gavnosp@hotmail.com>
2025-09-30 01:33:41 +1000

    Ignore pedantic rule

--- music_assistant/providers/deezer/__init__.py
+++ music_assistant/providers/deezer/__init__.py
@@ -842,4 +842,4 @@ class DeezerProvider(MusicProvider):
             Blowfish.MODE_CBC,
             b"\x00\x01\x02\x03\x04\x05\x06\x07",
         )
-        return cast(bytes, cipher.decrypt(chunk))
+        return cipher.decrypt(chunk)  # type: ignore[TC006]
