fe94cc7b05f585d5ba7f30898f268918431fa5df
John <john@munro.cc>
2025-10-04 15:32:05 +1300

    Update helpers.py
    Fix: for Chromecast group with stereo pair and another speaker becomes unavailable https://github.com/music-assistant/support/issues/4106

--- music_assistant/providers/chromecast/helpers.py
+++ music_assistant/providers/chromecast/helpers.py
@@ -82,7 +82,7 @@ class ChromecastInfo:
         self.is_dynamic_group = self.uuid in dynamic_groups
         if self.uuid in multichannel_groups:
             self.is_multichannel_group = True
-        elif multichannel_groups:
+        elif multichannel_groups and self.cast_type != "group" and cast_info.model_name != "Google Cast Group":
             self.is_multichannel_child = True
 
 
