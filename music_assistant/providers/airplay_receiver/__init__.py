1b213c98f3b0abe27b173ff48a1591697c77746b
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-11-06 03:40:51 +0100

    tweaks

--- music_assistant/providers/airplay_receiver/__init__.py
+++ music_assistant/providers/airplay_receiver/__init__.py
@@ -366,6 +366,12 @@ class AirPlayReceiverProvider(PluginProvider):
         """
         self.logger.debug("Received metadata update: %s", metadata)
 
+        # Handle metadata start (new track starting - clear old cover art)
+        if "metadata_start" in metadata:
+            if self._source_details.metadata:
+                self._source_details.metadata.image_url = None
+            return
+
         # Handle volume changes
         if "volume" in metadata and self._source_details.in_use_by:
             volume = metadata["volume"]
