4296a1ef02eee28125e35f06b5fcabf4c2b53127
Gav <gavnosp@hotmail.com>
2025-11-24 09:31:44 +1000

    Change base class

--- music_assistant/controllers/media/tracks.py
+++ music_assistant/controllers/media/tracks.py
@@ -599,19 +599,9 @@ class TracksController(MediaControllerBase[Track]):
             db_artist = existing
 
         if not db_artist or overwrite:
-            if isinstance(artist, ItemMapping):
-                # Can't add ItemMapping to library, must use existing
-                if not db_artist:
-                    msg = (
-                        f"Cannot add track artist: ItemMapping {artist.item_id} "
-                        "has no library entry"
-                    )
-                    raise InvalidDataError(msg)
-                # else: use the existing db_artist
-            else:
-                db_artist = await self.mass.music.artists.add_item_to_library(
-                    artist, overwrite_existing=overwrite
-                )
+            db_artist = await self.mass.music.artists.add_item_to_library(
+                artist, overwrite_existing=overwrite
+            )
         # write (or update) record in track_artists table
         await self.mass.music.database.insert_or_replace(
             DB_TABLE_TRACK_ARTISTS,
