87e6aeff3fea1a2f5d9fbd2df67a11adf6168bfa
Kieran Hogg <kieran.hogg@gmail.com>
2025-12-17 16:46:52 +0000

    Use 5 seconds for update interval

--- music_assistant/providers/bbc_sounds/__init__.py
+++ music_assistant/providers/bbc_sounds/__init__.py
@@ -278,6 +278,7 @@ class BBCSoundsProvider(MusicProvider):
         if episode_info and FEATURES["catchup_segments"]:
             stream_details.data = {"vpid": episode_info.id}
             stream_details.stream_metadata_update_callback = self._update_on_demand_stream_metadata
+            stream_details.stream_metadata_update_interval = 5
         return stream_details
 
     async def _get_station_stream_details(self, item_id: str) -> StreamDetails:
@@ -299,6 +300,7 @@ class BBCSoundsProvider(MusicProvider):
 
         if FEATURES["now_playing"]:
             stream_details.stream_metadata_update_callback = self._update_live_stream_metadata
+            stream_details.stream_metadata_update_interval = 5
         return stream_details
 
     async def get_stream_details(self, item_id: str, media_type: MediaType) -> StreamDetails:
