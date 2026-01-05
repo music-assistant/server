ff4ff6549fd303ab732f8b20800fa96a6cfdaae6
Marvin Schenkel <marvinschenkel@gmail.com>
2026-01-05 09:23:36 +0100

    Use bind server ip for sendspin url

--- music_assistant/controllers/webserver/remote_access/__init__.py
+++ music_assistant/controllers/webserver/remote_access/__init__.py
@@ -118,12 +118,18 @@ class RemoteAccessManager:
         mode = "optimized" if self._using_ha_cloud else "basic"
         self.logger.info("Starting remote access in %s mode", mode)
 
+        stream_bind_ip = (
+            "127.0.0.1" if self.mass.streams.bind_ip == "0.0.0.0" else self.mass.streams.bind_ip
+        )
+        sendspin_url = f"ws://{stream_bind_ip}:8927/sendspin"
+
         self.gateway = WebRTCGateway(
             http_session=self.mass.http_session,
             remote_id=self._remote_id,
             certificate=self._certificate,
             signaling_url=SIGNALING_SERVER_URL,
             local_ws_url=local_ws_url,
+            sendspin_url=sendspin_url,
             ice_servers=ice_servers,
             # Pass callback to get fresh ICE servers for each client connection
             # This ensures TURN credentials are always valid
--- music_assistant/controllers/webserver/sendspin_proxy.py
+++ music_assistant/controllers/webserver/sendspin_proxy.py
@@ -28,7 +28,6 @@ if TYPE_CHECKING:
     from music_assistant.controllers.webserver import WebserverController
 
 LOGGER = logging.getLogger(f"{MASS_LOGGER_NAME}.sendspin_proxy")
-INTERNAL_SENDSPIN_URL = "ws://127.0.0.1:8927/sendspin"
 
 
 class SendspinProxyHandler:
@@ -42,6 +41,17 @@ class SendspinProxyHandler:
         self.webserver = webserver
         self.mass = webserver.mass
         self.logger = LOGGER
+
+    @property
+    def internal_sendspin_url(self) -> str:
+        """Return the internal sendspin URL based on stream bind IP.
+
+        :return: The WebSocket URL for connecting to the internal Sendspin server.
+        """
+        stream_bind_ip = (
+            "127.0.0.1" if self.mass.streams.bind_ip == "0.0.0.0" else self.mass.streams.bind_ip
+        )
+        return f"ws://{stream_bind_ip}:8927/sendspin"
 
     async def handle_sendspin_proxy(self, request: web.Request) -> web.WebSocketResponse:
         """
@@ -86,7 +96,7 @@ class SendspinProxyHandler:
                 return wsock
 
         try:
-            internal_ws = await self.mass.http_session.ws_connect(INTERNAL_SENDSPIN_URL)
+            internal_ws = await self.mass.http_session.ws_connect(self.internal_sendspin_url)
         except Exception:
             self.logger.exception("Failed to connect to internal Sendspin server")
             await wsock.close(code=1011, message=b"Internal server error")
