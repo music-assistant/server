ba21ea350aa3e53a61ec0e1b43edb67ad39b206d
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-01-06 12:33:26 +0100

    be more generic in musiccast update

--- music_assistant/providers/musiccast/player.py
+++ music_assistant/providers/musiccast/player.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any, cast
 
 from aiohttp import ServerDisconnectedError
+from aiohttp.client_exceptions import ClientError
 from aiomusiccast.exceptions import MusicCastGroupException
 from aiomusiccast.pyamaha import MusicCastConnectionException
 from music_assistant_models.config_entries import ConfigEntry, ConfigValueOption, ConfigValueType
@@ -187,7 +188,11 @@ class MusicCastPlayer(Player):
                 _xml_media_info = await avt_get_media_info(
                     self.mass.http_session, self.physical_device
                 )
-            except ServerDisconnectedError:
+            except ClientError:
+                # this is regularly called, we can ignore a failing update
+                self.logger.debug("Acquiring media info failed, trying again in 5s.")
+                if self.upnp_update_helper is not None:
+                    self.upnp_update_helper.last_poll = now
                 return
             _player_current_url = search_xml(_xml_media_info, "CurrentURI")
 
