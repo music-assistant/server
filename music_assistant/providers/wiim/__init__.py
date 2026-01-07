c32db100a480071eac609a8477a864865f01e13f
David Anthoff <anthoff@berkeley.edu>
2026-01-06 23:53:25 -0800

    Fix a naming error in the WiiM provider

--- music_assistant/providers/wiim/__init__.py
+++ music_assistant/providers/wiim/__init__.py
@@ -39,10 +39,10 @@ async def setup(
 
 
 async def get_config_entries(
-    _mass: MusicAssistant,
-    _instance_id: str | None = None,
-    _action: str | None = None,
-    _values: dict[str, ConfigValueType] | None = None,
+    mass: MusicAssistant,  # noqa: ARG001
+    instance_id: str | None = None,  # noqa: ARG001
+    action: str | None = None,  # noqa: ARG001
+    values: dict[str, ConfigValueType] | None = None,  # noqa: ARG001
 ) -> tuple[ConfigEntry, ...]:
     """
     Return Config entries to setup this provider.
