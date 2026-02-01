098e0be4d1b6fe3a1468f8ad1aeea3f9acc5e17e
David Anthoff <anthoff@berkeley.edu>
2026-01-31 23:23:08 -0800

    Simplify Wiim code

--- music_assistant/providers/wiim/player.py
+++ music_assistant/providers/wiim/player.py
@@ -48,7 +48,7 @@ class WiimPlayer(Player):
         self.wiim_player = pywiim.Player(
             client,
             upnp_client=upnp_client,
-            on_state_changed=self._update_ma_state_from_sdk_cache,
+            on_state_changed=self.update_ma_state,
         )
         self.current_uri: str | None = None
 
@@ -59,15 +59,11 @@ class WiimPlayer(Player):
             self.wiim_upnp_client,  # Share same UPnP client
             self.wiim_player,  # Player implements apply_diff() for state updates
             self.player_id,
-            state_updated_callback=self.foo,
+            state_updated_callback=self.update_ma_state,
         )
 
         # Start UPnP event subscriptions
         await self.wiim_eventer.start()
-
-    def foo(self) -> None:
-        """Call the next status update method."""
-        self._update_ma_state_from_sdk_cache()
 
     @property
     def needs_poll(self) -> bool:
@@ -77,16 +73,11 @@ class WiimPlayer(Player):
     @property
     def poll_interval(self) -> int:
         """Return the interval in seconds to poll the player for state updates."""
-        # OPTIONAL
-        # used in conjunction with the needs_poll property.
-        # this should return the interval in seconds to poll the player for state updates.
         return 5
 
     async def poll(self) -> None:
         """Poll player for state updates."""
-        # OPTIONAL - This is called by the Player Manager if the 'needs_poll' property is True.
-        if self.wiim_player is not None:
-            await self.wiim_player.refresh()
+        await self.wiim_player.refresh()
 
     async def select_source(self, source: str) -> None:
         """
@@ -106,34 +97,28 @@ class WiimPlayer(Player):
 
     async def volume_set(self, volume_level: int) -> None:
         """Handle VOLUME_SET command on the player."""
-        if self.wiim_player is not None:
-            await self.wiim_player.set_volume(volume_level / 100.0)
+        await self.wiim_player.set_volume(volume_level / 100.0)
 
     async def volume_mute(self, muted: bool) -> None:
         """Handle VOLUME MUTE command on the player."""
-        if self.wiim_player is not None:
-            await self.wiim_player.set_mute(muted)
+        await self.wiim_player.set_mute(muted)
 
     async def play(self) -> None:
         """Play command."""
-        if self.wiim_player is not None:
-            await self.wiim_player.resume()
+        await self.wiim_player.resume()
 
     async def stop(self) -> None:
         """Stop command."""
-        if self.wiim_player is not None:
-            await self.wiim_player.stop()
+        await self.wiim_player.stop()
 
     async def pause(self) -> None:
         """Pause command."""
-        if self.wiim_player is not None:
-            await self.wiim_player.pause()
+        await self.wiim_player.pause()
 
     async def play_media(self, media: PlayerMedia) -> None:
         """Play media command."""
-        if self.wiim_player is not None:
-            await self.wiim_player.play_url(media.uri)
-            self.current_uri = media.uri
+        await self.wiim_player.play_url(media.uri)
+        self.current_uri = media.uri
 
     async def on_unload(self) -> None:
         """Handle logic when the player is unloaded from the Player controller."""
@@ -149,65 +134,61 @@ class WiimPlayer(Player):
         player_ids_to_remove: list[str] | None = None,
     ) -> None:
         """Handle SET_MEMBERS command on the player."""
-        if self.wiim_player is not None:
-            if player_ids_to_add:
-                for i in player_ids_to_add:
-                    child_player = cast("WiimPlayer", self.mass.players.get(i))
-                    if child_player is not None and child_player.wiim_player is not None:
-                        await child_player.wiim_player.join_group(self.wiim_player)
+        if player_ids_to_add:
+            for i in player_ids_to_add:
+                child_player = cast("WiimPlayer", self.mass.players.get(i))
+                await child_player.wiim_player.join_group(self.wiim_player)
 
-            if player_ids_to_remove:
-                for i in player_ids_to_remove:
-                    child_player = cast("WiimPlayer", self.mass.players.get(i))
-                    if child_player is not None and child_player.wiim_player is not None:
-                        await child_player.wiim_player.leave_group()
+        if player_ids_to_remove:
+            for i in player_ids_to_remove:
+                child_player = cast("WiimPlayer", self.mass.players.get(i))
+                await child_player.wiim_player.leave_group()
 
-    def _update_ma_state_from_sdk_cache(self) -> None:
+    def update_ma_state(self) -> None:
         """Update MA state from SDK's cache/HTTP poll attributes."""
-        if self.wiim_player is not None:
-            logger = self.logger
-            logger.debug("Device %s: Updating MA state from SDK cache/HTTP poll", self._attr_name)
+        logger = self.logger
+        logger.debug("Device %s: Updating MA state from SDK cache/HTTP poll", self._attr_name)
 
-            self._attr_available = self.wiim_player.available
+        self._attr_available = self.wiim_player.available
 
-            self._attr_volume_level = (
-                int(self.wiim_player.volume_level * 100)
-                if self.wiim_player.volume_level is not None
-                else None
+        self._attr_volume_level = (
+            int(self.wiim_player.volume_level * 100)
+            if self.wiim_player.volume_level is not None
+            else None
+        )
+        self._attr_volume_muted = self.wiim_player.is_muted
+
+        self._attr_playback_state = PlaybackState(self.wiim_player.state)
+
+        self._attr_elapsed_time = self.wiim_player.media_position
+        self._attr_elapsed_time_last_updated = time.time()
+
+        if self.wiim_player.is_master:
+            self._attr_group_members = (
+                [i.uuid for i in self.wiim_player.group.slaves if i.uuid is not None]
+                if self.wiim_player.group is not None
+                else []
             )
-            self._attr_volume_muted = self.wiim_player.is_muted
+        else:
+            self._attr_group_members.clear()
 
-            self._attr_playback_state = PlaybackState(self.wiim_player.state)
+        # for source in self.wiim_player.available_sources:
+        #     self._attr_source_list.append(PlayerSource(source.lower(), source))
 
-            self._attr_elapsed_time = self.wiim_player.media_position
-            self._attr_elapsed_time_last_updated = time.time()
-
-            if self.wiim_player.is_master:
-                self._attr_group_members = (
-                    [i.uuid for i in self.wiim_player.group.slaves if i.uuid is not None]
-                    if self.wiim_player.group is not None
-                    else []
-                )
+        if not self.wiim_player.is_slave:
+            if self.current_uri and self.current_uri == self.wiim_player.media_content_id:
+                self._attr_active_source = self.player_id
             else:
-                self._attr_group_members.clear()
+                self._attr_active_source = (
+                    self.wiim_player.source.lower() if self.wiim_player.source else ""
+                )
+                self.set_current_media(
+                    uri=self.wiim_player.media_content_id or "",
+                    title=self.wiim_player.media_title,
+                    artist=self.wiim_player.media_artist,
+                    album=self.wiim_player.media_album,
+                    image_url=self.wiim_player.media_image_url,
+                    duration=self.wiim_player.media_duration,
+                )
 
-            # for source in self.wiim_player.available_sources:
-            #     self._attr_source_list.append(PlayerSource(source.lower(), source))
-
-            if not self.wiim_player.is_slave:
-                if self.current_uri and self.current_uri == self.wiim_player.media_content_id:
-                    self._attr_active_source = self.player_id
-                else:
-                    self._attr_active_source = (
-                        self.wiim_player.source.lower() if self.wiim_player.source else ""
-                    )
-                    self.set_current_media(
-                        uri=self.wiim_player.media_content_id or "",
-                        title=self.wiim_player.media_title,
-                        artist=self.wiim_player.media_artist,
-                        album=self.wiim_player.media_album,
-                        image_url=self.wiim_player.media_image_url,
-                        duration=self.wiim_player.media_duration,
-                    )
-
-            self.update_state()
+        self.update_state()
