5c45727059499448e6571eb7f8956aa066f8abfd
Marvin Schenkel <marvinschenkel@gmail.com>
2026-02-09 15:44:44 +0100

    Change dict to ItemMapping

--- music_assistant/controllers/music.py
+++ music_assistant/controllers/music.py
@@ -876,12 +876,12 @@ class MusicController(CoreController):
 
     @api_command("music/library/add_item")
     async def add_item_to_library(
-        self, item: str | MediaItemType | dict[str, Any], overwrite_existing: bool = False
+        self, item: str | MediaItemType | ItemMapping, overwrite_existing: bool = False
     ) -> MediaItemType:
         """Add item (uri or mediaitem) to the library."""
-        if isinstance(item, dict):
-            # handle partial dicts from browse results
-            item = item["uri"]
+        if isinstance(item, ItemMapping):
+            # handle browse results that are returned as ItemMappings
+            item = item.uri
         # ensure we have a full item
         if isinstance(item, str):
             full_item = await self.get_item_by_uri(item)
