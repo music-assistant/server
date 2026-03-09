cf97f599c7373e61e7a1b91afdaf7b378e0eace5
Marvin Schenkel <marvinschenkel@gmail.com>
2026-03-09 13:21:56 +0100

    Fix 'Invalid PlayerMedia data' for plugin sources (#3341)

--- music_assistant/controllers/streams/streams_controller.py
+++ music_assistant/controllers/streams/streams_controller.py
@@ -384,6 +384,12 @@ class StreamsController(CoreController):
         """
         if media.media_type == MediaType.ANNOUNCEMENT:
             return media.uri
+        if media.media_type == MediaType.PLUGIN_SOURCE:
+            if media.custom_data and (source_id := media.custom_data.get("source_id")):
+                plugin_source = self.mass.players.get_plugin_source(source_id)
+                if plugin_source:
+                    return await self.get_plugin_source_url(plugin_source, player_id)
+            return media.uri
         protocol_player = self.mass.players.get_player(player_id)
         conf_output_codec = cast(
             "str",
