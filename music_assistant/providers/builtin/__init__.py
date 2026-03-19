f6726ec6ce6b4e422dc792a3ab5f8bc85af6616f
OzGav <gavnosp@hotmail.com>
2026-03-19 20:09:49 +1000

    Reduce playlist types

--- music_assistant/providers/builtin/__init__.py
+++ music_assistant/providers/builtin/__init__.py
@@ -550,7 +550,7 @@ class BuiltinProvider(MusicProvider):
         """
         parsed = urlparse(url)
         path_lower = parsed.path.lower()
-        is_playlist = path_lower.endswith((".pls", ".m3u", ".m3u8"))
+        is_playlist = path_lower.endswith((".pls"))
         if not is_playlist:
             return url
 
