556e83638bcb4a7c8cba6ee02e4d55d3ce474043
Marcel van der Veldt <m.vanderveldt@outlook.com>
2026-03-25 15:46:12 +0100

    Extend Party mode configuration (#3471)
    * Add additional config entries
    
    * tweak
    
    * default values
    
    * use action button to enable guest mode
    
    * more tweaks
    
    * make party plugin install by default (with player auto select)
    
    * fix review nitpicks
    
    * karaoke mode
    
    * Add immediate apply

--- music_assistant/constants.py
+++ music_assistant/constants.py
@@ -962,6 +962,7 @@ DEFAULT_PROVIDERS: Final[set[tuple[str, bool]]] = {
     ("sonos", True),
     ("bluesound", True),
     ("heos", True),
+    ("party", False),
 }
 
 EXTERNAL_SOURCES: Final[set[str]] = {
