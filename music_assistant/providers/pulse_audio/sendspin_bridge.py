39c276727a5d7b4deedffeb685e3301d24e1594a
iVolt1 <71899823+iVolt1@users.noreply.github.com>
2026-04-09 07:49:03 -0500

    Update sendspin_bridge.py

--- music_assistant/providers/pulse_audio/sendspin_bridge.py
+++ music_assistant/providers/pulse_audio/sendspin_bridge.py
@@ -96,10 +96,8 @@ class SendspinPulseAudioBridge:
                     SupportedAudioFormat(
                         codec=AudioCodec.PCM,
                         channels=BRIDGE_CHANNELS,
-                        sample_rate=96000,   # instead of BRIDGE_SAMPLE_RATE
-                        bit_depth=24,        # instead of BRIDGE_BIT_DEPTH
-                        #rate=BRIDGE_SAMPLE_RATE,
-                        #bit_depth=BRIDGE_BIT_DEPTH
+                        sample_rate=BRIDGE_SAMPLE_RATE,
+                        bit_depth=BRIDGE_BIT_DEPTH
                     )
                 ],
                 buffer_capacity=1_000,
