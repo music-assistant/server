574a8e73da338e91fd1d9415ba97084be4e3bf60
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-04-17 01:47:11 +0200

    Fix audiobook controller not using userid in library_items call (#3719)
    Small fix which ensures, that the in_progress/finished icons in the
    frontend refer to the correct user.
    
    Shall we backport this one?

--- music_assistant/controllers/media/audiobooks.py
+++ music_assistant/controllers/media/audiobooks.py
@@ -87,6 +87,9 @@ class AudiobooksController(MediaControllerBase[Audiobook]):
         """
         extra_query_params: dict[str, Any] = {}
         extra_query_parts: list[str] = []
+        extra_join_parts: list[str] = []
+        if session_user := get_current_user():
+            extra_join_parts = [f"AND playlog.userid = '{session_user.user_id}'"]
         result = await self.get_library_items_by_query(
             favorite=favorite,
             search=search,
@@ -97,6 +100,7 @@ class AudiobooksController(MediaControllerBase[Audiobook]):
             provider_filter=self._ensure_provider_filter(provider),
             extra_query_parts=extra_query_parts,
             extra_query_params=extra_query_params,
+            extra_join_parts=extra_join_parts,
             in_library_only=True,
         )
         if search and len(result) < 25 and not offset:
@@ -114,6 +118,7 @@ class AudiobooksController(MediaControllerBase[Audiobook]):
                 provider_filter=self._ensure_provider_filter(provider),
                 extra_query_parts=extra_query_parts,
                 extra_query_params=extra_query_params,
+                extra_join_parts=extra_join_parts,
                 in_library_only=True,
             )
         return result
