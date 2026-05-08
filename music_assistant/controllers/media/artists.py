4192e633add05d671748e9959131f01904275dbe
Marvin Schenkel <marvinschenkel@gmail.com>
2026-05-08 16:00:56 +0200

    Update music_assistant/controllers/media/artists.py
    Co-authored-by: Marcel van der Veldt <m.vanderveldt@outlook.com>

--- music_assistant/controllers/media/artists.py
+++ music_assistant/controllers/media/artists.py
@@ -67,7 +67,7 @@ class ArtistsController(MediaControllerBase[Artist]):
         # Try music providers mapped to the reference artist first.
         for prov_mapping in ref_item.provider_mappings:
             prov = self.mass.get_provider(prov_mapping.provider_instance)
-            if prov is None or not isinstance(prov, MusicProvider):
+            if not prov or not prov.available or not isinstance(prov, MusicProvider):
                 continue
             if ProviderFeature.SIMILAR_ARTISTS not in prov.supported_features:
                 continue
