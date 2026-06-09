bf7b2bb72acb894bd82d495724570a818b280fd2
Chris <chris@sicmedia.net>
2026-06-09 14:23:12 -0500

    Scope item-credit log to tracks (self-documenting, drops the inline comment)

--- music_assistant/controllers/music.py
+++ music_assistant/controllers/music.py
@@ -1320,11 +1320,8 @@ class MusicController(CoreController):
                 f"UPDATE {ctrl.db_table} SET play_count = play_count + 1, "
                 f"last_played = {timestamp} WHERE item_id = {db_item.item_id}"
             )
-            if not isinstance(media_item, Album):
-                # albums are already logged with their trigger context by the queue controller
-                self.logger.debug(
-                    "Credited play for %s '%s'", media_item.media_type.value, media_item.name
-                )
+            if isinstance(media_item, Track):
+                self.logger.debug("Credited play for track '%s'", media_item.name)
         if isinstance(media_item, Track | Album):
             await self._credit_artist_plays(
                 media_item.artists,
