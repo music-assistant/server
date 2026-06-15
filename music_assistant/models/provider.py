c5790c0c8c0aacefcc75f24760a585da687e13e7
Marcel van der Veldt <m.vanderveldt@outlook.com>
2026-06-15 23:54:17 +0200

    Trim Provider.to_dict() to match the ProviderInstance schema (#4239)

--- music_assistant/models/provider.py
+++ music_assistant/models/provider.py
@@ -172,13 +172,11 @@ class Provider:
             "type": self.type.value,
             "domain": self.domain,
             "name": self.name,
-            "default_name": self.default_name,
-            "instance_name_postfix": self.instance_name_postfix,
             "instance_id": self.instance_id,
-            "lookup_key": self.instance_id,  # include for backwards compatibility
             "supported_features": [x.value for x in self.supported_features],
             "available": self.available,
             "is_streaming_provider": getattr(self, "is_streaming_provider", None),
+            "lookup_key": self.instance_id,  # include for backwards compatibility
         }
 
     def supports_feature(self, feature: ProviderFeature) -> bool:
