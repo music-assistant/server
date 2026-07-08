94d59cc40b22849a724787bb9fa1724da7a08b01
Marcel van der Veldt <m.vanderveldt@outlook.com>
2026-07-08 23:49:17 +0200

    Add virtual player support to the Sendspin provider (#4666)

--- music_assistant/providers/sendspin/constants.py
+++ music_assistant/providers/sendspin/constants.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 BRIDGE_PREFIX = "spb_"
+VIRTUAL_PLAYER_ID_PREFIX = "sendspin_virtual_"
 
 CONF_CAST_AUDIO_UNSUPPORTED = "cast_audio_unsupported"
 CONF_SENDSPIN_STATIC_DELAY = "sendspin_static_delay"
+CONF_VIRTUAL_PLAYER_OWNER = "virtual_player_owner"
 DEFAULT_SENDSPIN_STATIC_DELAY = 0
