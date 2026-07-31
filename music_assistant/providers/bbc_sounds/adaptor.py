760e9c628de2474299fcd0aff1e3774488daec29
Kieran Hogg <kieran.hogg@gmail.com>
2026-07-31 11:50:57 +0100

    refactor: move some DEBUG output to VERBOSE

--- music_assistant/providers/bbc_sounds/adaptor.py
+++ music_assistant/providers/bbc_sounds/adaptor.py
@@ -49,6 +49,7 @@ from sounds.models import (
 )
 
 import music_assistant.helpers.datetime as dt
+from music_assistant.constants import VERBOSE_LOG_LEVEL
 from music_assistant.helpers.datetime import LOCAL_TIMEZONE
 from music_assistant.providers.bbc_sounds.constants import _Constants
 
@@ -809,7 +810,6 @@ class Adaptor:
             BrowseConverter(context),
             StationConverter(context),
             PodcastConverter(context),
-            BrowseConverter(context),
         ]
 
         for converter in converters:
@@ -875,10 +875,12 @@ class Adaptor:
             BrowseConverter(context),
             StationConverter(context),
             PodcastConverter(context),
-            BrowseConverter(context),
         ]
         for converter in converters:
-            self.logger.debug(f"Checking if converter {converter} can convert {type(source_obj)}")
+            self.logger.log(
+                VERBOSE_LOG_LEVEL,
+                f"Checking if converter {converter} can convert {type(source_obj)}",
+            )
             if converter.can_convert(source_obj):
                 try:
                     result = await converter.convert(source_obj)
@@ -890,12 +892,19 @@ class Adaptor:
                         f"Forced type to {context.force_type} but received {type(result)} "
                         f"using {type(converter)}"
                     )
-                self.provider.logger.debug(
-                    f"Successfully converted {type(source_obj).__name__}"
-                    f" to {type(result).__name__} {result}"
+                msg = (
+                    f"Successfully converted {type(source_obj).__name__} to {type(result).__name__}"
                 )
+                if hasattr(result, "item_id"):
+                    msg += f" item_id: {result.item_id}"
+                if hasattr(result, "urn"):
+                    msg += f" urn: {result.urn}"
+                self.logger.debug(msg)
+                self.provider.logger.log(VERBOSE_LOG_LEVEL, result)
                 return result
-            self.logger.debug(f"Converter {converter} could not convert {type(source_obj)}")
+            self.logger.log(
+                VERBOSE_LOG_LEVEL, f"Converter {converter} could not convert {type(source_obj)}"
+            )
 
         self.logger.warning(f"No converter found for type {type(source_obj).__name__}")
         self.logger.debug(str(source_obj))
