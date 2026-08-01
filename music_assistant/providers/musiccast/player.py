010eb507f1317a7a89d616d1d409061bbe791f76
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-08-01 10:48:24 +0200

    fix

--- music_assistant/providers/musiccast/player.py
+++ music_assistant/providers/musiccast/player.py
@@ -274,14 +274,16 @@ class MusicCastPlayer(Player):
 
         # STATE
 
+        self._attr_elapsed_time = None
         match self.zone_device.state:
             case MusicCastPlayerState.PAUSED:
                 self._attr_playback_state = PlaybackState.PAUSED
             case MusicCastPlayerState.PLAYING:
                 self._attr_playback_state = PlaybackState.PLAYING
+                self._attr_elapsed_time = self.zone_device.media_position
             case MusicCastPlayerState.IDLE | MusicCastPlayerState.OFF:
                 self._attr_playback_state = PlaybackState.IDLE
-        self._attr_elapsed_time = self.zone_device.media_position
+
         if self.zone_device.media_position_updated_at is not None:
             self._attr_elapsed_time_last_updated = (
                 self.zone_device.media_position_updated_at.timestamp()
