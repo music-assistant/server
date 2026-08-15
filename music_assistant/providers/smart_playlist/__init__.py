465e3e02323ae5af27938c9aa389f20ea1d8036f
DMoo <moos.daniel@googlemail.com>
2026-08-15 12:36:01 +0200

    Fix smart playlist removal from recommendations and add auto-refresh (#5641)

--- music_assistant/providers/smart_playlist/__init__.py
+++ music_assistant/providers/smart_playlist/__init__.py
@@ -21,6 +21,7 @@ from contextlib import suppress
 from dataclasses import replace as dc_replace
 from itertools import zip_longest
 from pathlib import Path
+from types import EllipsisType
 from typing import TYPE_CHECKING, Any
 
 from music_assistant_models.auth import Scope
@@ -319,7 +320,14 @@ class SmartPlaylistProvider(PluginProvider):
         if item_id != "smart_playlists":
             return items
         for pid, rules in self._rules_store.items():
-            items.append(await self._build_playlist(pid, rules))
+            library_item = await self.mass.music.playlists.get_library_item_by_prov_id(
+                pid, self.instance_id
+            )
+            if library_item:
+                items.append(ItemMapping.from_item(library_item))
+            else:
+                playlist = await self._build_playlist(pid, rules, library_item)
+                items.append(playlist)
         return items
 
     async def get_playlist(self, prov_playlist_id: str) -> Playlist:
@@ -441,6 +449,7 @@ class SmartPlaylistProvider(PluginProvider):
                 self._descriptions_store.pop(prov_id, None)
                 await self._invalidate_dynamic_sample_cache(prov_id)
                 await self._flush_rules_to_disk()
+                self.signal_provider_event({"event": "recommendations_updated"})
                 break
 
     async def _on_media_item_updated(self, event: MassEvent) -> None:
@@ -493,6 +502,7 @@ class SmartPlaylistProvider(PluginProvider):
         library_playlist = await self.mass.music.playlists.add_item_to_library(playlist)
         self.mass.metadata.schedule_update_metadata(library_playlist)
         self._schedule_ai_description_refresh(playlist_id)
+        self.signal_provider_event({"event": "recommendations_updated"})
         return library_playlist
 
     async def generate_playlist(
@@ -690,7 +700,12 @@ class SmartPlaylistProvider(PluginProvider):
             self.logger.debug("Could not resolve playlist id %s: %s", playlist_id, err)
         return None
 
-    async def _build_playlist(self, playlist_id: str, rules: SmartPlaylistRules) -> Playlist:
+    async def _build_playlist(
+        self,
+        playlist_id: str,
+        rules: SmartPlaylistRules,
+        library_item: Playlist | None | EllipsisType = ...,
+    ) -> Playlist:
         """Build a Playlist object from stored rules."""
         name = self._names_store.get(playlist_id, playlist_id)
         playlist = Playlist(
@@ -712,15 +727,18 @@ class SmartPlaylistProvider(PluginProvider):
         playlist.is_dynamic = rules.is_dynamic
         playlist.metadata = MediaItemMetadata(
             description=self._description_for(playlist_id, rules),
-            images=await self._images_for(playlist_id),
+            images=await self._images_for(playlist_id, library_item),
         )
         return playlist
 
-    async def _images_for(self, playlist_id: str) -> UniqueList[MediaItemImage]:
+    async def _images_for(
+        self, playlist_id: str, library_item: Playlist | None | EllipsisType = ...
+    ) -> UniqueList[MediaItemImage]:
         """Return images for the playlist from the library, or empty list if none available."""
-        library_item = await self.mass.music.playlists.get_library_item_by_prov_id(
-            playlist_id, self.instance_id
-        )
+        if library_item is ...:
+            library_item = await self.mass.music.playlists.get_library_item_by_prov_id(
+                playlist_id, self.instance_id
+            )
         if library_item and library_item.metadata and library_item.metadata.images:
             return library_item.metadata.images
         return UniqueList([])
