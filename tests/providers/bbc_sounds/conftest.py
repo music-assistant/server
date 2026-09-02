c80884bbe41f7bb691b580ee4403265ba0c83e41
Kieran Hogg <kieran.hogg@gmail.com>
2026-09-02 19:16:50 +0100

    test: add some menu fixtures

--- tests/providers/bbc_sounds/conftest.py
+++ tests/providers/bbc_sounds/conftest.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from unittest.mock import Mock
 
 import pytest
+from sounds.models import Menu, MenuItem
 
 from music_assistant.providers.bbc_sounds import SUPPORTED_FEATURES, BBCSoundsProvider
 from music_assistant.providers.bbc_sounds.adaptor import Adaptor
@@ -27,3 +28,41 @@ def provider() -> BBCSoundsProvider:
     instance.logged_in = True
     instance.adaptor = Adaptor(instance)
     return instance
+
+
+@pytest.fixture
+def blank_menu() -> Menu:
+    """Create a blank menu."""
+    return Menu(sub_items=[])
+
+
+@pytest.fixture
+def uk_menu() -> Menu:
+    """Create a realistic UK menu."""
+    return Menu(
+        sub_items=[
+            MenuItem(id="listen_live", title="Listen Live"),
+            MenuItem(id="stations", title="Station & Schedules"),
+            MenuItem(id="continue_listening", title="Continue Listening"),
+            MenuItem(id="latest_news", title="Latest News Playlist"),
+            MenuItem(id="editors_picks", title="Editor's Picks"),
+            MenuItem(id="from_your_area", title="From Your Area"),
+            MenuItem(id="collections", title="Collections"),
+            MenuItem(id="categories", title="Categories"),
+            MenuItem(id="explore_all", title="Explore All"),
+        ]
+    )
+
+
+@pytest.fixture
+def international_menu() -> Menu:
+    """Create a realistic international menu."""
+    return Menu(
+        sub_items=[
+            MenuItem(id="listen_live", title="Listen Live"),
+            MenuItem(id="stations", title="Station & Schedules"),
+            MenuItem(id="collections", title="Collections"),
+            MenuItem(id="categories", title="Categories"),
+            MenuItem(id="explore_all", title="Explore All"),
+        ]
+    )
