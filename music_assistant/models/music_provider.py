17547848269b33626a508c60b210acba0b2cc7cb
Moos, Daniel <daniel.moos@juliusbaer.com>
2026-09-07 15:35:48 +0200

    Polish external ID lookup

--- music_assistant/models/music_provider.py
+++ music_assistant/models/music_provider.py
@@ -12,7 +12,7 @@ from datetime import datetime
 from typing import TYPE_CHECKING, Final, cast
 
 from music_assistant_models.background_task import TaskSchedule
-from music_assistant_models.enums import ArtistType, MediaType, ProviderFeature
+from music_assistant_models.enums import ArtistType, ExternalID, MediaType, ProviderFeature
 from music_assistant_models.errors import (
     AudioError,
     InvalidDataError,
@@ -664,19 +664,19 @@ class MusicProvider(Provider):
         raise NotImplementedError
 
     async def get_track_by_external_id(
-        self, external_id: str, external_id_type: str
+        self, external_id: str, external_id_type: ExternalID
     ) -> Track | None:
         """Retrieve track by external ID (ISRC, MusicBrainz, etc.)."""
         raise NotImplementedError
 
     async def get_album_by_external_id(
-        self, external_id: str, external_id_type: str
+        self, external_id: str, external_id_type: ExternalID
     ) -> Album | None:
         """Retrieve album by external ID (MusicBrainz, Discogs, etc.)."""
         raise NotImplementedError
 
     async def get_artist_by_external_id(
-        self, external_id: str, external_id_type: str
+        self, external_id: str, external_id_type: ExternalID
     ) -> Artist | None:
         """Retrieve artist by external ID (MusicBrainz, etc.)."""
         raise NotImplementedError
