430d4e10d8f67e874fec648fe5b330c6934cfcb3
Adam <afeldman1@gmail.com>
2026-09-19 12:48:50 -0400

    Use global constants

--- music_assistant/providers/audiobookshelf/constants.py
+++ music_assistant/providers/audiobookshelf/constants.py
@@ -16,7 +16,6 @@ STREAMDETAILS_EXPIRATION_S = 300
 CONF_URL = "url"
 CONF_OLD_TOKEN = "token"
 CONF_API_TOKEN = "api_token"  # with jwt api token (>= v2.26)
-CONF_VERIFY_SSL = "verify_ssl"
 # optionally hide podcasts with no episodes
 CONF_HIDE_EMPTY_PODCASTS = "hide_empty_podcasts"
 
